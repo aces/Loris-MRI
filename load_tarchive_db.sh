7948681b0d6cea05129fb4ee073966a97c4ca24e
zia <zia.mohades@gmail.com>
2012-08-30 17:21:58 -0400

    get the project prefix from config file

--- load_tarchive_db.sh
+++ load_tarchive_db.sh
@@ -8,9 +8,16 @@ echo "Usage: $0 <site>"
 exit 1
 fi
 
+
+#####Get config setting#######################################################
+{ package Settings; do "$ENV{HOME}/.neurodb/prod" }
+my $prefix         = $Settings::prefix;
+#######################################################################################
+
+
 tempdir=$TMPDIR/load_tarchive_db.$$
 mkdir -p $tempdir
-cp /data/incoming/${site}IBIS/incoming/tarchive_data.sql.gz $tempdir/
+cp /data/incoming/${site}$prefix/incoming/tarchive_data.sql.gz $tempdir/
 gunzip $tempdir/tarchive_data.sql.gz
 mysql --defaults-file=/home/ibis/mriscript.my.cnf -e "DELETE FROM tarchive WHERE neurodbCenterName='${site}'"
 mysql --defaults-file=/home/ibis/mriscript.my.cnf < $tempdir/tarchive_data.sql
