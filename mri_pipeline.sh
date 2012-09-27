37558e5a78477136734e533f27450bc21df0b947
zia <zia.mohades@gmail.com>
2012-09-27 16:00:15 -0400

    Modifies the data directory permission and modifies the email address (by adding a \ slash before @)

--- mri_pipeline.sh
+++ mri_pipeline.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 ################################
 ####WHAT WILL NOT DO#############
@@ -15,7 +15,10 @@ stty echo
 
 read -p "what is the linux user which the intallation will be based on? " USER
 read -p "what is the project Name " PROJ   ##this will be used to create all the corresponding directories...i.e /data/gusto/bin.....
+
 read -p "what is your email address " email
+email=${email/@/\\@}  ##adds a back slash before the @
+##echo "email is $email" 
 read -p "Enter the list of Site names (space separated) " site
 mridir=`pwd`
 ##read -p "Enter Full Loris-code directory path "   lorisdir
@@ -79,10 +82,14 @@ echo
 ######################change permissions ##########################################
 ####################################################################################
 echo "Changing permissions"
-sudo chown $USER:$USER /home/$USER/.neurodb/
-sudo chmod -R  750 /home/$USER/.neurodb/
-sudo chown $USER:$USER /data/$PROJ/
+sudo chown -R $USER:$USER /home/$USER/.neurodb/
+sudo chown -R $USER:$USER /data/$PROJ/
+sudo chown -R $USER:$USER /data/incoming/
+
+sudo chmod -R 750 /home/$USER/.neurodb/
 sudo chmod -R 750 /data/$PROJ/
+sudo chmod -R 750 /data/incoming/
+
 echo
 ######################################################################################
 ##########################change the prod file#######################################
