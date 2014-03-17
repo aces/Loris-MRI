6a6249d0036264da5d043859e9bcfb7fb671cc25
zia <zia.mohades@gmail.com>
2014-03-17 11:08:33 -0400

    Fixed typos and indentation

--- updateMRI_Upload.pl
+++ updateMRI_Upload.pl
@@ -32,8 +32,6 @@ my $globArchiveLocation = 0;   # whether to use strict ArchiveLocation strings
 
 my $Help = <<HELP;
 *******************************************************************************
-Minc Insertion 
-*******************************************************************************
 
 Author  :   
 Date    :   
@@ -46,7 +44,6 @@ The program does the following:
 ====explain====
 
 HELP
-
 
 my $Usage = "------------------------------------------
 $0 updates the mri_upload table to populate the fields
@@ -83,6 +80,7 @@ if (-f "$ENV{HOME}/.neurodb/$profile") {
         package Settings; do "$ENV{HOME}/.neurodb/$profile" 
     }
 }
+
 if ($profile && !defined @Settings::db) {
     print "\n\tERROR: You don't have a configuration file named '$profile' in:
             $ENV{HOME}/.neurodb/ \n\n"; 
@@ -92,12 +90,10 @@ if ($profile && !defined @Settings::db) {
 ################################################################
 ################# if profile not specified######################
 ################################################################
-
 if(!$profile) { 
     print $Usage; print "\n\tERROR: You must specify an existing profile.\n\n";  
     exit 3;  
 }
-
 
 ################################################################
 ################# if tarchive not specified#####################
@@ -108,7 +104,6 @@ unless (-e $tarchive) {
     exit 4;
 }
 
-
 ################################################################
 ################# if source locatio is not set##################
 ################################################################
@@ -118,8 +113,6 @@ unless (-e $source_location) {
            exit now.\n\n\n";
     exit 5;
 }
-
-
 ################################################################
 #####establish database connection if database option is set####
 ################################################################
@@ -129,20 +122,17 @@ print "Connecting to database.\n" if $verbose;
 ################################################################
 #####Check to see if the tarchiveid is already set or not#######
 ################################################################
-
 my $where = " WHERE t.ArchiveLocation = '$tarchive'";
 if ($globArchiveLocation) {
     $where = " WHERE t.ArchiveLocation LIKE '%/".basename($tarchive)."'";
 }
 $query  = "SELECT COUNT(*) FROM mri_upload m
                          JOIN tarchive t on (t.TarchiveID=m.TarchiveID) $where";
-##print $query . "\n";
 my $count = $dbh->selectrow_array($query);
 if($count>0) {
    print "\n\tERROR: the tarchive is already uploaded \n\n"; 
    exit 6;
 } 
-
 
 ################################################################
 #####get the tarchiveid from tarchive table#####################
@@ -158,7 +148,6 @@ my $tarchiveID = $dbh->selectrow_array($query);
 ################################################################
 #####populate the mri_upload with the correct values############
 ################################################################
-
 $query = "INSERT INTO mri_upload SET UploadedBy='$User', UploadDate=NOW() , 
           TarchiveID ='$tarchiveID' , SourceLocation='$source_location'";
 $dbh->do($query);
