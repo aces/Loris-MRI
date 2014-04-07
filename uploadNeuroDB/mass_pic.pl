035de310a538e856cc6c4ffd29d4921029cdd955
zia <zia.mohades@gmail.com>
2014-04-07 16:17:50 -0400

    Removed 'Use warnings'

--- uploadNeuroDB/mass_pic.pl
+++ uploadNeuroDB/mass_pic.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 use strict;
-use warnings;
 use FindBin;
 use lib "$FindBin::Bin";
 use Getopt::Tabular;
--- uploadNeuroDB/tarchive_validation.pl
+++ uploadNeuroDB/tarchive_validation.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/perl
 use strict;
-use warnings;
 use Carp;
 use Getopt::Tabular;
 use FileHandle;
