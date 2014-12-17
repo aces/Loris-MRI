095d094b3f9c22a2b3f88526ba6d2886fba154cc
Cécile Madjar <cecile.madjar@gmail.com>
2014-12-17 14:33:05 -0500

    Added installation guide lines and script for mac os x

---
+++ INSTALL.Mac.md
@@ -0,0 +1,44 @@
+# 1. Request Loris-MRI Github repository permission, fork it to your Git-user, then clone the fork to your server
+```
+sudo mkdir -p /data/$PROJ/bin
+sudo chown -R lorisadmin:lorisadmin /data/$PROJ
+cd /data/$PROJ/bin
+git clone git@github.com:your-git-username/Loris-MRI.git mri
+```
+Note: $PROJ = project name
+
+# 2. Install Dicom-archive within the mri/ directory
+```
+cd /data/$PROJ/bin/mri/
+git submodule init
+git submodule sync
+git submodule updatemo9
+```
+Note: $PROJ = project name
+
+# 3. Install [minctoolkit](http://www.bic.mni.mcgill.ca/ServicesSoftware/MINC) and [dcmtk] (http://dicom.offis.de/dcmtk.php.en)
+
+# 4. Install the following perl libraries
+Note: Before compiling DBD::mysql, you will need to create some alias because MySQL on Mac is installed differently than on Linux
+```
+cd /usr/local
+sudo mkdir lib
+cd lib
+sudo ln -s /usr/local/mysql/lib/*.dylib .
+```
+```
+sudo -S cpan install Math::Round
+sudo -S cpan install Getopt::Tabular
+sudo -S cpan install Time::JulianDay
+sudo -S cpan install Path::Class
+sudo -S cpan install DBI
+sudo -S cpan install DBD::mysql
+```
+# 5. Install md5sum library
+```
+sudo port install md5sh1sum
+```
+# 6. Run imaging_install_MacOSX.sh script
+```
+sh imaging_install_MacOSX.sh
+```
---
+++ imaging_install_MacOSX.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+################################
+####WHAT WILL NOT DO#############
+###1)It doesn't set up the SGE
+###2)It doesn't fetch the CIVET stuff   TODO:Get the CIVET stuff from somewhere and place it in h
+###3)It doesn't change the config.xml
+
+
+#Create a temporary log for installation and delete it on completion 
+#@TODO make sure that /tmp is writable
+LOGFILE="/tmp/$(basename $0).$$.tmp"
+touch $LOGFILE
+trap "rm  $LOGFILE" EXIT
+ 
+read -p "what is the database name? " mysqldb
+read -p "What is the database host? " mysqlhost
+read -p "What is the Mysql user? " mysqluser
+stty -echo ##this disables the password to show up on the commandline
+read -p "What is the mysql password? " mysqlpass; echo
+stty echo
+
+read -p "what is the linux user which the installation will be based on? " USER
+read -p "what is the project Name " PROJ   ##this will be used to create all the corresponding directories...i.e /data/gusto/bin.....
+
+read -p "what is your email address " email
+email=${email/@/\\\\@}  ##adds a back slash before the @
+echo "email is $email"
+
+
+read -p "what prod file name would you like to use? default: prod? " prodfilename
+if [ -z "$prodfilename" ]; then
+    prodfilename="prod"
+fi 
+ 
+read -p "Enter the list of Site names (space separated) " site
+mridir=`pwd`
+
+##########################################################################################
+#############################Create directories########################################
+#########################################################################################
+ echo "Creating the data directories"
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/"
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/trashbin"   ##holds mincs that didn't match protocol
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/tarchive"   ##holds tared dicom-folder
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/pic"           ##holds jpegs generated for the MRI-browser
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/logs"         ## holds logs from pipeline script
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/jiv"            ## holds JIVs used for JIV viewer
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/assembly" ## holds the MINC files
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/batch_output"  ##contains the result of the SGE (queue
+  sudo -S su $USER -c "mkdir -p $mridir/dicom-archive/.loris_mri"
+echo
+#######################################################################################
+ ###############incoming directory using sites########################################
+#######################################################################################
+ echo "Creating incoming director(y/ies)"
+  for s in $site; do 
+   sudo -S su $USER -c "mkdir -p /data/incoming/$s/incoming";
+  done;
+ echo
+
+####################################################################################
+#######set environment variables under .bashrc#####################################
+###################################################################################
+echo "Modifying environment script"
+sed -i "s#%PROJECT%#$PROJ#g" $mridir/environment
+##Make sure that CIVET stuff are placed in the right place
+##source  /data/$PROJ/bin/$mridirname/environment
+export TMPDIR=/tmp
+echo
+
+####################################################################################
+######################change permissions ##########################################
+####################################################################################
+#echo "Changing permissions"
+
+sudo chmod -R 750 $mridir/.loris_mri/
+sudo chmod -R 750 /data/$PROJ/
+sudo chmod -R 750 /data/incoming/
+echo
+
+
+echo
+######################################################################################
+##########################change the prod file#######################################
+#####################################################################################
+echo "Creating MRI config file"
+
+cp $mridir/dicom-archive/profileTemplate $mridir/dicom-archive/.loris_mri/$prodfilename
+sudo chmod 640 $mridir/dicom-archive/.loris_mri/$prodfilename
+
+sed -e "s#project#$PROJ#g" -e "s#/PATH/TO/DATA/location#/data/$PROJ/data#g" -e "s#yourname\\\@example.com#$email#g" -e "s#/PATH/TO/get_dicom_info.pl#$mridir/dicom-archive/get_dicom_info.pl#g"  -e "s#DBNAME#$mysqldb#g" -e "s#DBUSER#$mysqluser#g" -e "s#DBPASS#$mysqlpass#g" -e "s#DBHOST#$mysqlhost#g" -e "s#/PATH/TO/dicomlib/#/data/$PROJ/data/tarchive#g" $mridir/dicom-archive/profileTemplate > $mridir/dicom-archive/.loris_mri/$prodfilename
+echo "config file is located at $mridir/dicom-archive/.loris_mri/$prodfilename"
+echo
+
