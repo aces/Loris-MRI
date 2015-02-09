694b3dc45ba99014d9a63b5c0bc737baf21768ed
zia <zia.mohades@gmail.com>
2015-02-09 13:57:28 -0500

    Modifications

--- uploadNeuroDB/NeuroDB/ImagingUpload.pm
+++ uploadNeuroDB/NeuroDB/ImagingUpload.pm
@@ -6,7 +6,7 @@ use warnings;
 use Data::Dumper;
 use File::Basename;
 use Path::Class;
-use NeuroDB::FileDecompress;
+use FileDecompress;
 use File::Temp qw/ tempdir /;
 
 =pod
@@ -23,6 +23,7 @@ todo:
 ###which will be in a temp folder i.e /tmp folder
 ###once the validation passes the File will be moved to a
 ### final destination directory
+################################################################
 sub new {
     my $params = shift;
     my ($dbhr,$temp_file_path,$pname) = @_;
@@ -48,58 +49,77 @@ sub new {
 }
 
 
-###############################################################################
-##############################setEnvironment###################################
-###############################################################################
+#################################################################
+##############################setEnvironment#####################
+#################################################################
 sub setEnvironment {
+  my $this = shift;
   my $environment_file = $Settings::data_dir . "/" . "environment";
-  $environment_set = $this->runCommand("source $environment_file");
+  my $command = "source $environment_file";
+  $this->runCommand($command);
 
 }
-################################################################
-#####################IsValid##################################
-################################################################
-###Validates the File to be uploaded############################
+#################################################################
+#####################IsValid#####################################
+#################################################################
+###Validates the File to be uploaded#############################
 ####if the validation passes the following will happen:
 ####1) Copy the file from tmp folder to the /data/incoming
 ####2) Set the isvalidated to true in the mri_upload table
 
-################################################################
+#################################################################
 
 sub IsValid  {
     my $this = shift;
-    my $file_decompress = NeuroDB::FileDecompress->new(
+    my $message = '';
+    my $file_decompress = FileDecompress->new(
 			$this->{'temp_file_path'}
                      );
+    #################################################
     ####Get a list of files from the archive
+    #################################################
     my @files = $file_decompress->getArchivedFiles();	
     my $files_not_dicom = 0;
     my $files_with_unmatched_patient_name = 0;
     my $is_valid = 0;     
+    #################################################
     #############Loop through the files##############
+    #################################################
     foreach (@files) {
-=pod
-          1) Check to see if it's dicom
-          2) Check to see if the header matches the patient-name            
-=cut    
+
+    ############################################################
+    ###  1) Check to see if it's dicom##########################
+    ###  2) Check to see if the header matches the patient-name#
+    ############################################################
+    
     	if (!isDicom($_)) {
-		$files_not_dicom++;
+    		$files_not_dicom++;
         }
         if (!PatientNameMatch($_)) {
- 		$files_with_unmatched_patient_name++;
-	}
+ 		    $files_with_unmatched_patient_name++;
+     	}
     }
-   if (($files_not_dicom > 0) || 
-      ($files_with_unmatched_patient_name>0)) {
-	$is_valid= 0;
+
+   if ($files_not_dicom > 0)  {
+       $message = "\n ERROR: there are $files_not_dicom files which are " .
+                   "Are not DiCOM";
+       print ($message);
+       return 0;
+
    }
-   return  $is_valid;
+   if ($files_with_unmatched_patient_name>0) {
+        $message = "\n ERROR: there are $files_with_unmatched_patient_name files".
+                   " where the patient-name doesn't match ";
+     print ($message);
+     return 0;
+   }
+   return 1; ##return true
 }
 
 
-###############################################################################
-###############################runDicomTar#####################################
-###############################################################################
+#################################################################
+###############################runDicomTar#######################
+#################################################################
 sub runDicomTar {
     my $this = shift;
     my $tarchive_location = $Settings::data_dir. "/" . "tarchive";
@@ -107,94 +127,99 @@ sub runDicomTar {
     my $command = "perl $dicomtar" . $this->{'temp_file_path'} .   
 	"$tarchive_location -clobber -database -profile prod";
     my $output = $this->runCommand($command);
-    $output = $output >> 8;
+    $output = $output >> 8; ##returns the exit code
     return $output;
 }
 
-###############################################################################
-###############################runInsertingScripts#############################
-###############################################################################
+##################################################################
+###############################runInsertingScripts################
+##################################################################
 sub runInsertionScripts {
   my $this = shift;
   my $archived_file_path = $this->getTarchiveFileLocation();
   my $command = $Settings::bin_dir. "/uploadNeuroDB/tarchiveLoader" . 
 		"-globLocation -profile prod $archived_file_path";
   my $output = $this->runCommand($command);
-  $output = $output >> 8;
+  $output = $output >> 8; ##returns the exit code
   return $output;
 }
 
-###############################################################################
-###############################getgetArchivedFiles#############################
-###############################################################################
+#################################################################
+###############################getgetArchivedFiles###############
+#################################################################
 sub getArchivedFiles {
   my $this = shift;
   my $files = ${$this->{'extract_object'}}->files;
   return $files;
 }
-###############################################################################
-###############################getType#########################################
-###############################################################################
+#################################################################
+###############################getType###########################
+#################################################################
 
 sub getType {
   my $this = shift;
   my $type = ${$this->{'extract_object'}}->type;
   return $type;
 }
-##################################################################################
-###############################PatientNameMatch###################################
-##################################################################################
+#################################################################
+###############################PatientNameMatch##################
+#################################################################
 sub PatientNameMatch {
  my $this = shift;
  my ($dicom_file) = @_;
- $cmd = "dcmdump $file | grep -i patientname";
+ my $cmd = "dcmdump $dicom_file | grep -i patientname";
 
- $patient_name_string = $this->runCommand($cmd);
+ my $patient_name_string = $this->runCommand($cmd);
  my ($l,$pname,$t) = split /^\[(.*?)\]^/, $patient_name_string;
- if ($pname eq  $this->{'pname'}) {
-	return 1;
+ if ($pname ne  $this->{'pname'}) {
+     my $message = "The patient-name $pname does not Match" .
+                $this->{'pname'};
+     print $message;
+     return 0; ##return false
  }
- return  0;
+ return 1; ##return true
 
 }
-##################################################################################
-###############################If DICOM File######################################
-##################################################################################
+################################################################
+###############################If DICOM File####################
+################################################################
 sub isDicom {
  my $this = shift;
  my ($dicom_file) = @_;
- $file_type = $this->runCommand("file $dicom_file") ;
+ my $file_type = $this->runCommand("file $dicom_file") ;
  if ($file_type =~/DICOM/) {
-	return 1;
+    print "not of type DICOM";
+    return 0;
  }
- return 0;
+ return 1;
 }
 
 
-##################################################################################
-###############################getTarchiveFileLocation############################
-##################################################################################
+################################################################
+###################getTarchiveFileLocation######################
+################################################################
 sub getTarchiveFileLocation {
 	my $this = shift;
 	my $archive_location  = '';
-	 $query = "SELECT t.ArchiveLocation FROM mri_upload m 
+	my $query = "SELECT t.ArchiveLocation FROM mri_upload m 
 	   JOIN t tarchive ON (m.TarchiveID = t.TarchiveID)
 	   WHERE m.SourceLocation =?";
-	${$this->{'dbhr'}}->execute($this->{'temp_file_path'};
-	if ($sth->rows> 0)) {
+	my $sth = ${$this->{'dbhr'}}->prepare($query);
+    $sth->execute($this->{'temp_file_path'});
+	if ($sth->rows> 0) {
 		$archive_location = $sth->fetchrow_array();
 	}
 	return $archive_location; 
 }
-##################################################################################
-###############################moveUploadedFile###################################
-##################################################################################
+################################################################
+###############################moveUploadedFile#################
+################################################################
 sub moveUploadedFile {
 }
 
-##################################################################################
-###############################runCommand#########################################
-##################################################################################
+################################################################
+###############################runCommand#######################
+################################################################
 
 sub runCommand {
  my $this = shift;
