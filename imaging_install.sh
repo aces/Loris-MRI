d791b15bcbb45f5f2c0ec8aa74cc60366b04c20e
Jkat <fission@live.com>
2015-04-16 14:55:24 -0400

    Making the install script less disgusting

--- imaging_install.sh
+++ imaging_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-################################
-####WHAT WILL NOT DO#############
-###1)It doesn't set up the SGE
-###2)It doesn't fetch the CIVET stuff   TODO:Get the CIVET stuff from somewhere and place it in h
-###3)It doesn't change the config.xml
-
+##################################
+###WHAT THIS SCRIPT WILL NOT DO###
+#1)It doesn't set up the SGE
+#2)It doesn't fetch the CIVET stuff   TODO:Get the CIVET stuff from somewhere and place it in somewhere
+#3)It doesn't change the config.xml
+##################################
 
 #Create a temporary log for installation and delete it on completion 
 #@TODO make sure that /tmp is writable
@@ -14,113 +14,111 @@ touch $LOGFILE
 trap "rm  $LOGFILE" EXIT
  
 
-## First, check that all required modules are installed.
-## Check if cpan module installed
+#First, check that all required modules are installed.
+#Check if cpan module installed
 CPANCHECK=`which cpan`
 if [ ! -f "$CPANCHECK" ]; then
     echo "\nERROR: Unable to find cpan"
-    echo "Please, ask your sysadmin to install CPAN\n"
+    echo "Please ask your sysadmin or install CPAN\n"
     exit
 fi
-## Check if make is installed
+#Check if make is installed
 MAKECHECK=`which make`
 if [ ! -f "$MAKECHECK" ]; then
     echo "\nERROR: Unable to find make"
-    echo "Please, ask your sysadmin to install MAKE\n"
+    echo "Please ask your sysadmin or install MAKE\n"
     exit
 fi
-## Check if apt-get is install
+#Check if apt-get is install
 APTGETCHECK=`which apt-get`
 if [ ! -f "$APTGETCHECK" ]; then
     echo "\nERROR: Unable to find apt-get"
-    echo "Please, ask your sysadmin to install APT-GET\n"
+    echo "Please ask your sysadmin or install APT-GET\n"
     exit
 fi
 
 
-read -p "what is the database name? " mysqldb
+read -p "What is the database name? " mysqldb
 read -p "What is the database host? " mysqlhost
-read -p "What is the Mysql user? " mysqluser
-stty -echo ##this disables the password to show up on the commandline
-read -p "What is the mysql password? " mysqlpass; echo
+read -p "What is the MySQL user? " mysqluser
+stty -echo
+read -p "What is the MySQL password? " mysqlpass; echo
 stty echo
-
-read -p "what is the linux user which the installation will be based on? " USER
-read -p "what is the project Name " PROJ   ##this will be used to create all the corresponding directories...i.e /data/gusto/bin.....
-
-read -p "what is your email address " email
+read -p "What is the Linux user which the installation will be based on? " USER
+read -p "What is the project name? " PROJ   ##this will be used to create all the corresponding directories...i.e /data/gusto/bin.....
+read -p "What is your email address? " email
 email=${email/@/\\\\@}  ##adds a back slash before the @
-echo "email is $email"
-
-
-read -p "what prod file name would you like to use? default: prod? " prodfilename
+read -p "What prod file name would you like to use? default: prod " prodfilename
 if [ -z "$prodfilename" ]; then
     prodfilename="prod"
 fi 
  
 read -p "Enter the list of Site names (space separated) " site
 mridir=`pwd`
-##read -p "Enter Full Loris-code directory path "   lorisdir
+#read -p "Enter Full Loris-code directory path "   lorisdir
 
-#################################################################################################
+################################################################################################
 ########################################MINC TOOL###############################################
-##################################################################################################
-echo "installing Minc toolkit (May prompt for sudo password)"
+################################################################################################
+echo "Installing Minc toolkit (May prompt for sudo password)"
 sudo -S apt-get install minc-tools
 echo
 
-echo "installing dicom toolkit (May prompt for sudo password)"
+echo "Installing dicom toolkit (May prompt for sudo password)"
 sudo -S apt-get install dcmtk
 echo
+
 #################################################################################################
-############################INSTALL THE PERL LIBRARIES############################################
+############################INSTALL THE PERL LIBRARIES###########################################
 #################################################################################################
 echo "Installing the perl libraries...THis will take a few minutes..."
-##echo $rootpass | sudo perl -MCPAN -e shell
+#echo $rootpass | sudo perl -MCPAN -e shell
 sudo -S cpan install Math::Round
-##echo $rootpass | sudo -S cpan install Bundle::CPAN
+#echo $rootpass | sudo -S cpan install Bundle::CPAN
 sudo -S cpan install Getopt::Tabular
 sudo -S cpan install Time::JulianDay
 sudo -S cpan install Path::Class
 sudo -S cpan install Archive::Extract;
 sudo -S cpan install Archive::Zip;
 echo
-##########################################################################################
+
+#######################################################################################
 #############################Create directories########################################
-#########################################################################################
- echo "Creating the data directories"
+#######################################################################################
+echo "Creating the data directories"
   sudo -S su $USER -c "mkdir -p /data/$PROJ/data/"
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/trashbin"   ##holds mincs that didn't match protocol
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/tarchive"   ##holds tared dicom-folder
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/pic"           ##holds jpegs generated for the MRI-browser
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/logs"         ## holds logs from pipeline script
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/jiv"            ## holds JIVs used for JIV viewer
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/assembly" ## holds the MINC files
-  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/batch_output"  ##contains the result of the SGE (queue
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/trashbin"         #holds mincs that didn't match protocol
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/tarchive"         #holds tared dicom-folder
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/pic"              #holds jpegs generated for the MRI-browser
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/logs"             #holds logs from pipeline script
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/jiv"              #holds JIVs used for JIV viewer
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/assembly"         #holds the MINC files
+  sudo -S su $USER -c "mkdir -p /data/$PROJ/data/batch_output"     #contains the result of the SGE (queue
   sudo -S su $USER -c "mkdir -p $mridir/dicom-archive/.loris_mri"
 echo
-#######################################################################################
- ###############incoming directory using sites########################################
-#######################################################################################
-sudo -S su $USER -c "mkdir -p /data/incoming/";
- echo "Creating incoming director(y/ies)"
-  for s in $site; do 
-   sudo -S su $USER -c "mkdir -p /data/incoming/$s/incoming";
-  done;
- echo
 
-####################################################################################
+#####################################################################################
+###############incoming directory using sites########################################
+#####################################################################################
+sudo -S su $USER -c "mkdir -p /data/incoming/";
+echo "Creating incoming director(y/ies)"
+ for s in $site; do 
+  sudo -S su $USER -c "mkdir -p /data/incoming/$s/incoming";
+ done;
+echo
+
+###################################################################################
 #######set environment variables under .bashrc#####################################
 ###################################################################################
 echo "Modifying environment script"
 sed -i "s#%PROJECT%#$PROJ#g" $mridir/environment
-##Make sure that CIVET stuff are placed in the right place
-##source  /data/$PROJ/bin/$mridirname/environment
+#Make sure that CIVET stuff are placed in the right place
+#source /data/$PROJ/bin/$mridirname/environment
 export TMPDIR=/tmp
 echo
 
 ####################################################################################
-######################change permissions ##########################################
+######################change permissions ###########################################
 ####################################################################################
 #echo "Changing permissions"
 
@@ -132,8 +130,6 @@ echo
 ####################################################################################
 ######################Add the proper Apache group user #############################
 ####################################################################################
-
-
 if egrep ^www-data: /etc/group > $LOGFILE 2>&1;
 then 
     group=www-data
@@ -147,13 +143,12 @@ else
     read -p "Cannot find the apache group name for your installation. Please provide? " group
 fi
 
-# Setting group permissions 
+#Setting group permissions 
 sudo chgrp $group -R /data/$PROJ/data/
 sudo chgrp $group -R /data/incoming/
-
-
 echo
-######################################################################################
+
+#####################################################################################
 ##########################change the prod file#######################################
 #####################################################################################
 echo "Creating MRI config file"
@@ -165,8 +160,7 @@ sed -e "s#project#$PROJ#g" -e "s#/PATH/TO/DATA/location#/data/$PROJ/data#g" -e "
 echo "config file is located at $mridir/dicom-archive/.loris_mri/$prodfilename"
 echo
 
-
 ######################################################################
-###########Modify the config.xml########################################
+###########Modify the config.xml######################################
 ######################################################################
-##sed -i "s#SAME AS imagePath#/data/$PROJ/data#g" -i "s#/PATH/TO/MINC/DATA/ROOT/mri-data/minc/#data/$PROJ/data#g" $lorisdir/project/config.xml
+#sed -i "s#SAME AS imagePath#/data/$PROJ/data#g" -i "s#/PATH/TO/MINC/DATA/ROOT/mri-data/minc/#data/$PROJ/data#g" $lorisdir/project/config.xml
