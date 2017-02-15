019e18c7a4f40a17e171c5a9fc30226b197fc1fa
Gregory Luneau <gluneau.mcin@gmail.com>
2017-02-15 12:08:45 -0500

    added option to also re-insert

--- tools/deletemincsqlwrapper.pl
+++ tools/deletemincsqlwrapper.pl
@@ -8,26 +8,35 @@ use File::Copy;
 use Term::ANSIColor qw(:constants);
 use NeuroDB::DBI;
 
-my $profile = "prod";
+my $profile = "null_grads"; # "prod";
 { package Settings; do "$ENV{LORIS_CONFIG}/.loris_mri/" . $profile}
 my $dbh = &NeuroDB::DBI::connect_to_db(@Settings::db);
 
+
+if (!empty($argv[1]) {
+  if ($argv[1] eq "insertminc") {
+    my $insertminc = 1;
+  }
+}
+
 # Only the f.SeriesUID is really needed for minc_deletion, other fields are for information only
 my $queryF = <<SQL;
-  select f.fileid, f.SeriesUID, f.SessionID, f.file, from_unixtime(f.InsertTime), p.Value, q.QCStatus, c.Alias, m.Scan_type
-  from files as f 
-  left join parameter_file as p using (FileID)
-  left join parameter_type as t using (ParameterTypeID)
-  left join files_qcstatus as q using (FileID)
-  left join session as s on (f.SessionID=s.ID)
-  left join psc as c on (c.CenterID=s.CenterID)
-  left join mri_scan_type as m on (m.ID=f.AcquisitionProtocolID)
-  where t.Name = 'acquisition:slice_thickness'
-  and p.Value like '%4.%'
-  and (m.Scan_type like '%t1%' or m.Scan_type like '%t2%')
-  order by f.InsertTime
+    SELECT DISTINCT
+    f.FileID, f.File, f.SeriesUID, t.ArchiveLocation, SUBSTRING_INDEX(t.ArchiveLocation, '/', -1) as tarchive
+    FROM files AS f
+    LEFT JOIN session s ON (f.SessionID=s.ID)
+    LEFT JOIN parameter_file AS pf USING (FileID)
+    LEFT JOIN files_qcstatus AS fq USING (FileID)
+    LEFT JOIN tarchive AS t ON f.TarchiveSource=t.TarchiveID
+    WHERE f.SeriesUID='1.3.12.2.1107.5.2.32.35177.2015072521545163128256406.0.0.0' or f.SeriesUID='1.3.12.2.1107.5.2.32.35177.2015072822492087585653675.0.0.0' and
+    ((pf.ParameterTypeID=329 and pf.`VALUE` like '%acquisition:direction_x = 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.%')
+     or (pf.ParameterTypeID=329 and pf.`VALUE` like '%acquisition:direction_y = 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.%')
+     or (pf.ParameterTypeID=329 and pf.`VALUE` like '%acquisition:direction_z = 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.%')
+     or (pf.ParameterTypeID=333 and pf.`VALUE` like '%0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.%')
+     or (pf.ParameterTypeID=334 and pf.`VALUE` like '%0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.%')
+     or (pf.ParameterTypeID=343 and pf.`VALUE` like '%0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.%'))
+    ORDER BY f.FileID
 SQL
-
 
 my $sthF = $dbh->prepare($queryF);
 
@@ -69,5 +78,11 @@ while ($fF = $sthF->fetchrow_hashref()) {
     my $minc_delete_log = `$minc_delete_cmd`;
     print $minc_delete_log . "\n";
 
+    if ($insertminc) {
+      my $tar_loader_cmd  = "../uploadNeuroDB/tarchiveLoader -profile " . $profile . " -seriesuid " . $fF->{'SeriesUID'} . " -verbose -globLocation " . $fF->{'ArchiveLocation'};
+      print $tar_loader_cmd . "\n";
+      my $tar_loader_log  = `$tar_loader_cmd`;
+      print $tar_loader_log . "\n";
+    }
   }
 }
