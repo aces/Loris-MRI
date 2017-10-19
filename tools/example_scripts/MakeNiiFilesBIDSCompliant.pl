da72bcceb9a4a3001584716c2af82d99b58a2326
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-10-19 17:01:51 -0400

    PO long set of comments addressed

--- tools/example_scripts/MakeNiiFilesBIDSCompliant.pl
+++ tools/example_scripts/MakeNiiFilesBIDSCompliant.pl
@@ -6,9 +6,12 @@ use Getopt::Tabular;
 use File::Path qw/ make_path /;
 use File::Basename;
 use NeuroDB::DBI;
+use JSON;
 
 my $profile = undef;
 my $tarchiveID = undef;
+my $BIDSVersion = "1.0.2";
+my $datasetName = undef;
 
 my @opt_table = (
     [ "-profile", "string", 1, \$profile,
@@ -16,14 +19,20 @@ my @opt_table = (
     ],
     [ "-tarchive_id", "string", 1, \$tarchiveID,
       "tarchive_id of the .tar to be processed from tarchive table"
+    ],
+    [ "-dataset_name", "string", 1, \$datasetName,
+      "Name/Description of the dataset about to be generated in BIDS format; for example CCNA_First_Sample_Data"
     ]
 ); 
 
 my $Help = <<HELP;
 
-This script will remove the root directory from the ArchiveLocation field
-in the tarchive table to make path to the tarchive relative. This should 
-be used once, when updating the LORIS-MRI code.
+This script will create a BIDS compliant NII file structure of the minc files
+currently present in the assembly/ directory. If tarchive_id is specified,
+only the images from that archive will be processed, otherwise, when no
+tarchive_id is specified, all files in assembly/ will be included in the BIDS
+structure.
+Requires JSON library for Perl. Run sudo apt-get install libjson-perl to get it.
 
 HELP
 
@@ -44,6 +53,14 @@ if ($profile && !@Settings::db) {
     print "\n\tERROR: You don't have a configuration file named ".
           "'$profile' in:  $ENV{LORIS_CONFIG}/.loris_mri/ \n";
     exit 2;
+}
+
+if ( !$datasetName ) {
+    print $Help;
+    print "$Usage\n\tERROR: The dataset name needs to be provided. "
+      . "it is required by BIDS specifications to populate the "
+      . "dataset_description.json file \n\n";
+    exit 3;
 }
 
 ################################################################
@@ -67,6 +84,21 @@ $dataDir    =~ s/\/$//g;
 # same level as assmebly/ directory but named as BIDS/
 my $destDir = $dataDir . "/BIDS";
 make_path($destDir) unless(-d  $destDir);
+
+# Create the dataset_description.json file
+my $dataDescFileName = "dataset_description.json";
+my $dataDescFile     = $destDir . "/" . $dataDescFileName;
+unless (-e $dataDescFile) {
+    print "\n*******Creating the dataset description file*******\n";
+    open DATADESCINFO, ">$dataDescFile";
+    DATADESCINFO->autoflush(1);
+    select(DATADESCINFO);
+    select(STDOUT);
+    my %dataset_desc_hash = ('BIDSVersion' => $BIDSVersion, 'Name' => $datasetName);
+    my $json = encode_json \%dataset_desc_hash;
+    print DATADESCINFO "$json\n";
+    close DATADESCINFO;
+}
 
 my ($query, $sth);
 
@@ -203,27 +235,20 @@ Input:  - $dbh = database handler
 sub makeNIIAndHeader {
     
     my ( $dbh, %file_list) = @_;
-    my ($row, $scanCategory, $destDirFinal);
+    my ($row, $scanType, $BIDSCategory, $BIDSScanType, $destDirFinal);
     foreach $row (keys %file_list) {
         my $fileID            = $file_list{$row}{'fileID'};
         my $minc              = $file_list{$row}{'file'};
         my $fileAcqProtocolID = $file_list{$row}{'AcquisitionProtocolID'};
         my $candID            = $file_list{$row}{'candID'};
-        my $visitLabel        = $file_list{$row}{'visitLabel'};
-        my $mincBase          = basename($minc);
-        my $remove            = $prefix . "_" . $candID . "_" . $visitLabel;  
-        my $replace           = "sub-" . $candID . "_ses-" . $visitLabel ; 
-        my $nifti             = $mincBase;
-        $nifti                =~ s/mnc/nii/g;
-        $nifti                =~ s/$remove/$replace/g;
-
-        # for files that have DTI in their names, replace it with DWI as BIDS expects that
-        $nifti                =~ s/dti/dwi/g;
+        my $visitLabelOrig    = $file_list{$row}{'visitLabel'};
 
         # Get the scan category (anat, func, dwi, to know which subdirectory to place files in
         ( my $query = <<QUERY ) =~ s/\n/ /g;
 SELECT
-  Scan_category
+  Scan_type,
+  BIDS_category,
+  BIDS_Scan_type
 FROM
   mri_scan_type mst
 WHERE
@@ -232,8 +257,47 @@ QUERY
         # Prepare and execute query
         my $sth = $dbh->prepare($query);
         $sth->execute($fileAcqProtocolID);
-        $scanCategory = $sth->fetchrow_array();
-        $destDirFinal = $destDir . "/sub-" . $candID . "/ses-" . $visitLabel . "/" . $scanCategory;
+        my $rowhr = $sth->fetchrow_hashref();
+        $scanType     = $rowhr->{'Scan_type'};
+        $BIDSCategory = $rowhr->{'BIDS_category'};
+        $BIDSScanType = $rowhr->{'BIDS_Scan_type'};
+
+        my $mincBase          = basename($minc);
+        my $nifti             = $mincBase;
+
+        # Make extension nii instead of minc
+        $nifti                =~ s/mnc/nii/g;
+
+        # If Visit Label contains underscores, remove them
+        my $visitLabel        = $visitLabelOrig;
+        $visitLabel           =~ s/_//g;
+
+        # Remove prefix; i.e project name; and add sub- and ses- in front of CandID and Visit label
+        my $remove            = $prefix . "_" . $candID . "_" . $visitLabelOrig;
+        my $replace           = "sub-" . $candID . "_ses-" . $visitLabel;
+        $nifti                =~ s/$remove/$replace/g;
+
+        # make the filename have the BIDS Scan type name, in case the project Scan type name is not compliant;
+        # and append the word 'run' before run number
+        $remove = $scanType . "_";
+        $replace = $BIDSScanType . "_run-";
+        $nifti =~ s/$remove/$replace/g;
+
+        # If the file is functional; need to add a trailing 'bold' before the extension
+        if ($BIDSCategory eq 'func') {
+            # find position of the last dot, so where the extension starts
+            my ($base,$path,$ext) = fileparse($nifti, qr{\..*});
+            $base = $base . "_bold";
+            $nifti = $base . $ext;
+        }
+        # If the file is functional; need to add a leading 'task' before BIDS scan type
+        if ($BIDSCategory eq 'func') {
+            $remove = $BIDSScanType;
+            $replace = "task-" . $BIDSScanType;
+            $nifti =~ s/$remove/$replace/g;
+        }
+
+        $destDirFinal = $destDir . "/sub-" . $candID . "/ses-" . $visitLabel . "/" . $BIDSCategory;
         make_path($destDirFinal) unless(-d  $destDirFinal);
 
         my $mincFullPath = $dataDir . "/" . $minc;
@@ -251,7 +315,9 @@ QUERY
             system($gzip_cmd);
 
             #  create json information from minc files header; 
-            my (@headerNameArr, $headerNameArr, @headerNameDBArr, $headerName, $headerNameDB, $headerVal, $headerFile);
+            my (@headerNameArr, $headerNameArr,
+                @headerNameDBArr, $headerNameDBArr,
+                $headerName, $headerNameDB, $headerVal, $headerFile);
             # Name as it appears in the database
             @headerNameDBArr      = ("repetition_time","manufacturer","manufacturer_model_name","magnetic_field_strength",
                                 "device_serial_number","software_versions","acquisition:receive_coil","acquisition:scanning_sequence",
@@ -260,13 +326,17 @@ QUERY
             @headerNameArr  = ("RepetitionTime","Manufacturer","ManufacturerModelName","MagneticFieldStrength",
                                 "DeviceSerialNumber","SoftwareVersions","ReceiveCoilName","PulseSequenceType",
                                 "EchoTime","InversionTime","SliceTiming", "FlipAngle", "InstitutionName");
+
             $headerFile         = $nifti;
             $headerFile         =~ s/nii/json/g;
             open HEADERINFO, ">$destDirFinal/$headerFile";
             HEADERINFO->autoflush(1);
             select(HEADERINFO);
             select(STDOUT);
-            print HEADERINFO "{\n";
+
+            my %header_hash;
+            my $header_hash;
+            my $currentHeaderJSON;
    
             foreach my $j (0..scalar(@headerNameDBArr)-1) {
                 $headerNameDB = $headerNameDBArr[$j];
@@ -293,18 +363,111 @@ QUERY
                 $sth->execute($headerNameDB,$fileID);
                 if ( $sth->rows > 0 ) {
                     $headerVal = $sth->fetchrow_array();
-                    print HEADERINFO "$headerName: $headerVal,\n";
+                    $header_hash{$headerName} = $headerVal;
                     print "     $headerName was found for $nifti with value $headerVal\n";
                 }
                 else {
                     print "     $headerName was not found for $nifti\n";
                 }
             }
-            print HEADERINFO "}\n";
+            $currentHeaderJSON = encode_json \%header_hash;
+            print HEADERINFO "$currentHeaderJSON";
             close HEADERINFO;
         }
         else {
             print "\nCould not find the following minc file: $mincFullPath\n";
+        }
+
+        # DWI files need 2 extra special files; .bval and .bvec
+        if ($BIDSScanType eq 'dwi') {
+            my ( @headerNameBVALDBArr, $headerNameBVALDBArr,
+                 @headerNameBVECDBArr, $headerNameBVECDBArr,
+                 $headerName, $headerNameDB, $headerVal, $bvalFile, $bvecFile);
+            @headerNameBVALDBArr      = ("acquisition:bvalues");
+            @headerNameBVECDBArr      = ("acquisition:direction_x","acquisition:direction_y","acquisition:direction_z");
+
+            #BVAL first
+            $bvalFile         = $nifti;
+            $bvalFile         =~ s/nii/bval/g;
+            open BVALINFO, ">$destDirFinal/$bvalFile";
+            BVALINFO->autoflush(1);
+            select(BVALINFO);
+            select(STDOUT);
+            foreach my $j (0..scalar(@headerNameBVALDBArr)-1) {
+                $headerNameDB = $headerNameBVALDBArr[$j];
+                $headerNameDB =~ s/^\"+|\"$//g;
+                print "Adding now $headerName header to $bvalFile\n";
+                ( $query = <<QUERY ) =~ s/\n/ /g;
+SELECT
+  pf.Value
+FROM
+  parameter_file pf
+JOIN
+  files f
+ON
+  pf.FileID=f.FileID
+WHERE
+  pf.ParameterTypeID = (SELECT pt.ParameterTypeID from parameter_type pt WHERE pt.Name = ?)
+AND
+  f.FileID = ?
+QUERY
+                # Prepare and execute query
+                $sth = $dbh->prepare($query);
+                $sth->execute($headerNameDB,$fileID);
+                if ( $sth->rows > 0 ) {
+                    $headerVal = $sth->fetchrow_array();
+                    $headerVal =~ s/\.\,//g;
+                    # There is one last trailing .; remove it
+                    $headerVal =~ s/\.$//g;
+                    print BVALINFO "$headerVal \n";
+                    print "     $headerNameDB was found for $nifti with value $headerVal\n";
+                }
+                else {
+                    print "     $headerNameDB was not found for $nifti\n";
+                }
+            }
+            close BVALINFO;
+
+            #BVEC next
+            $bvecFile         = $nifti;
+            $bvecFile         =~ s/nii/bvec/g;
+            open BVECINFO, ">$destDirFinal/$bvecFile";
+            BVECINFO->autoflush(1);
+            select(BVECINFO);
+            select(STDOUT);
+            foreach my $j (0..scalar(@headerNameBVECDBArr)-1) {
+                $headerNameDB = $headerNameBVECDBArr[$j];
+                $headerNameDB =~ s/^\"+|\"$//g;
+                print "Adding now $headerName header to $bvecFile\n";
+                ( $query = <<QUERY ) =~ s/\n/ /g;
+SELECT
+  pf.Value
+FROM
+  parameter_file pf
+JOIN
+  files f
+ON
+  pf.FileID=f.FileID
+WHERE
+  pf.ParameterTypeID = (SELECT pt.ParameterTypeID from parameter_type pt WHERE pt.Name = ?)
+AND
+  f.FileID = ?
+QUERY
+                # Prepare and execute query
+                $sth = $dbh->prepare($query);
+                $sth->execute($headerNameDB,$fileID);
+                if ( $sth->rows > 0 ) {
+                    $headerVal = $sth->fetchrow_array();
+                    $headerVal =~ s/\.\,//g;
+                    $headerVal =~ s/\,//g;
+                    print BVECINFO "$headerVal \n";
+                    print "     $headerNameDB was found for $nifti with value $headerVal\n";
+                }
+                else {
+                    print "     $headerNameDB was not found for $nifti\n";
+                }
+            }
+            close BVECINFO;
         }
     }
 }
