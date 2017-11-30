37a6734a01f29ccf19f2aed7df7a8eb1b1eb4a80
MounaSafiHarab <mouna.safiharab@gmail.com>
2017-11-30 15:13:30 -0500

    Dave and Nick feedback

--- uploadNeuroDB/NeuroDB/ImagingUpload.pm
+++ uploadNeuroDB/NeuroDB/ImagingUpload.pm
@@ -110,7 +110,7 @@ sub IsCandidateInfoValid {
     ####Get a list of files from the folder#####################
     #############Loop through the files#########################
     ############################################################
-    my $cmd = "cd " . $this->{'uploaded_temp_folder'} . "; find -name '__MACOSX' -exec rm -r {} + ";
+    my $cmd = "find -path " . $this->{'uploaded_temp_folder'} . " -name '__MACOSX' -delete ";
     print "\n $cmd \n";
     system($cmd);
 
