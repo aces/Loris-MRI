3aa0721cc67468becd29d37f3687e6de1a58e80d
Cécile <cecile.madjar@gmail.com>
2017-12-15 08:53:33 -0500

    First iteration of perldoc

--- uploadNeuroDB/NeuroDB/DBI.pm
+++ uploadNeuroDB/NeuroDB/DBI.pm
@@ -1,53 +1,64 @@
-# ------------------------------ MNI Header ----------------------------------
-#@NAME       : NeuroDB::DBI
-#@DESCRIPTION: Perform common tasks relating to database connectivity within the NeuroDB system
-#@EXPORT     : look at the @EXPORT line below ;P
-#@EXPORT_OK  : none
-#@EXPORT_TAGS: none
-#@USES       : Exporter, DBI (with DBD::mysql)
-#@REQUIRES   : 
-#@VERSION    : $Id: DBI.pm,v 1.3 2006/09/21 18:45:25 moi Exp $
-#@CREATED    : 2003/03/18, Jonathan Harlap
-#@MODIFIED   : sebas
-#@COPYRIGHT  : Copyright (c) 2003 by Jonathan Harlap, McConnell Brain Imaging
-#              Centre, Montreal Neurological Institute, McGill University.
-#-----------------------------------------------------------------------------
-
 package NeuroDB::DBI;
 
+=pod
+
+=head1 NAME
+
+NeuroDB::DBI
+
+
+=head1 SYNOPSIS
+
+use NeuroDB::DBI;
+
+my $dbh = &NeuroDB::DBI::connect_to_db(@Settings::db);
+
+my $sth = $dbh->prepare($query);
+$sth->execute(@bind_value);
+
+my @row_array  = $sth->fetchrow_array;
+
+
+=head1 DESCRIPTION
+
+This package performs common tasks relating to database connectivity between
+the LORIS-MRI code base and the LORIS backend database. The following methods
+are available.
+
+=cut
+
+
+use strict;
+use warnings;
 use Exporter ();
 use DBI;
+use Pod::Usage;
 
-$VERSION = 0.1;
-@ISA = qw(Exporter);
+=pod
 
-@EXPORT = qw(connect_to_db);
-@EXPORT_OK = qw();
+=head2 connect_to_db
 
-# ------------------------------ MNI Header ----------------------------------
-#@NAME       : connect_to_db
-#@INPUT      : optional: database, username, password, host
-#@OUTPUT     : 
-#@RETURNS    : DBI database handle
-#@DESCRIPTION: connects to database (default: qnts_dev) on host (default montague) 
-#              as username & password (default: mriscript)
-#@METHOD     : 
-#@GLOBALS    : 
-#@CALLS      : 
-#@CREATED    : 2003/03/18, Jonathan Harlap
-#@MODIFIED   : sebas
-#-----------------------------------------------------------------------------
+This methods connects to the LORIS database ($db_database) on host ($db_host)
+as username ($db_user) & password ($db_pass).
+
+C<connect_to_db($db_name, $db_user, $db_pass, $db_host);>
+
+INPUT: optional: database, username, password, host
+
+RETURNS: DBI database handler.
+
+=cut
 sub connect_to_db
 {
     my ($db_name, $db_user, $db_pass, $db_host) = @_;
     
-    $db_name=""   unless $db_name;           # database name
-    $db_host=""   unless $db_host;           # host name
+    $db_name=""   unless $db_name;          # database name
+    $db_host=""   unless $db_host;          # host name
     my $db_port="3306";
 
     my $db_dsn = "DBI:mysql:database=$db_name;host=$db_host;port=$db_port;";
-    $db_user = ""    unless $db_user;               # user name (fill in as appropriate)
-    $db_pass = ""    unless $db_pass;               # password (ditto)
+    $db_user = ""    unless $db_user;       # user name (fill in as appropriate)
+    $db_pass = ""    unless $db_pass;       # password (ditto)
 
     my $dbh = DBI->connect($db_dsn, $db_user, $db_pass) or die "DB connection failed\nDBI Error: ". $DBI::errstr."\n";
     return $dbh;
@@ -55,3 +66,15 @@ sub connect_to_db
 
 
 1;
+
+=pod
+
+=head1 AUTHOR
+
+Jonathan Harlap, McConnell Brain Imaging Centre, Montreal Neurological
+Institute, McGill University.
+
+
+=head1 COPYRIGHT AND LICENSE
+
+=cut
