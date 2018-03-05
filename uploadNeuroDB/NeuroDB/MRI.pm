4a54ed5accab74451e076333126d7764678c743a
MounaSafiHarab <mouna.safiharab@gmail.com>
2018-03-05 17:16:36 -0500

    MRI protocol table can no longer have comma separated ranges: Redmine xyz

--- uploadNeuroDB/NeuroDB/MRI.pm
+++ uploadNeuroDB/NeuroDB/MRI.pm
@@ -649,8 +649,8 @@ sub scan_type_text_to_id {
 B<in_range( C<$value>, C<$range_string> )>
 
 determines whether numerical value falls within the range described by
-range string.  range string is a comma-seperated list of range units.
-a single range unit follows the syntax either "X" or "X-Y"
+range string.  range string is a single range unit which follows the 
+syntax "X" or "X-Y"
 
 Returns: 1 if the value is in range, 0 otherwise
 
@@ -664,19 +664,20 @@ sub in_range
     return 0 unless $range_string;
     return 0 unless defined($value);
 
-    my @ranges = split(/,/, $range_string);
 
-    my $range = 0;
-    foreach $range (@ranges) {
-       chomp($range);
-       if($range=~/^[0-9.]+$/) { ## single value element
-           return 1 if &floats_are_equal($value, $range, $FLOAT_EQUALS_NB_DECIMALS);
-       } else { ## range X-Y
-           $range =~ /([0-9.]+)-([0-9.]+)/;
-           return 1 if ($1 <= $value && $value <= $2) 
-               || &floats_are_equal($value, $1, $FLOAT_EQUALS_NB_DECIMALS) 
-               || &floats_are_equal($value, $2, $FLOAT_EQUALS_NB_DECIMALS);
-       }
+    if($range_string=~ /,/) {
+        print "Comma separated ranges, as found in $range_string, are no longer supported\n";
+        return 0;
+    }
+
+    chomp($range_string);
+    if($range_string=~/^[0-9.]+$/) { ## single value element
+        return 1 if &floats_are_equal($value, $range_string, $FLOAT_EQUALS_NB_DECIMALS);
+    } else { ## range X-Y
+        $range_string =~ /([0-9.]+)-([0-9.]+)/;
+        return 1 if ($1 <= $value && $value <= $2) 
+            || &floats_are_equal($value, $1, $FLOAT_EQUALS_NB_DECIMALS) 
+            || &floats_are_equal($value, $2, $FLOAT_EQUALS_NB_DECIMALS);
     }
 
     ## if we've gotten this far, we're out of range.
