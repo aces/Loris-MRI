9bc1dfd531a410b1e9352fabc6aebd0656947ed3
Cecile Madjar <cecile.madjar@gmail.com>
2018-03-16 12:44:13 -0400

    Duh... Counting is so hard apparently... Don't know what I was on but it appears to be some pretty good stuff!!

--- docs/scripts_md/ExitCodes.md
+++ docs/scripts_md/ExitCodes.md
@@ -34,36 +34,36 @@ Below is a list of the possible exit codes organized per script:
 1\. Common exit codes to most insertion scripts (exit codes from 0 to 19, 0 =
 exit script with success status)
 
-2\. Exit codes from batch\_uploads\_imageuploader (exit codes from 20 to 49)
+2\. Exit codes from batch\_uploads\_imageuploader (exit codes from 20 to 39)
 
 3\. Exit codes from batch\_uploads\_tarchive (no exit codes available yet, exit
-codes will be from 60 to 69)
+codes will be from 40 to 59)
 
-4\. Exit codes from dicom-archive/dicomTar.pl (exit codes from 80 to 89)
+4\. Exit codes from dicom-archive/dicomTar.pl (exit codes from 60 to 79)
 
-5\. Exit codes from dicom-archive/updateMRI\_upload (exit codes from 100 to 119)
+5\. Exit codes from dicom-archive/updateMRI\_upload (exit codes from 80 to 99)
 
-6\. Exit codes from DTIPrep/DTIPrep\_pipeline.pl (exit codes from 120 to 139)
+6\. Exit codes from DTIPrep/DTIPrep\_pipeline.pl (exit codes from 100 to 119)
 
-7\. Exit codes from DTIPrep/DTIPrepRegister.pl (exit codes from 140 to 159)
+7\. Exit codes from DTIPrep/DTIPrepRegister.pl (exit codes from 120 to 139)
 
-8\. Exit codes from uploadNeuroDB/imaging\_upload\_file.pl (exit codes from 160
+8\. Exit codes from uploadNeuroDB/imaging\_upload\_file.pl (exit codes from 140
+to 159)
+
+9\. Exit codes from uploadNeuroDB/NeuroDB/ImagingUpload.pm (exit codes from 160
 to 179)
 
-9\. Exit codes from uploadNeuroDB/NeuroDB/ImagingUpload.pm (exit codes from 180
-to 199)
-
 10\. Exit codes from uploadNeuroDB/NeuroDB/MRIProcessingUtility.pm (exit codes
- from 200 to 219)
+ from 180 to 199)
 
-11\. Exit codes from uploadNeuroDB/minc\_deletion.pl (exit codes from 220 to 239)
+11\. Exit codes from uploadNeuroDB/minc\_deletion.pl (exit codes from 200 to 219)
 
-12\. Exit codes from uploadNeuroDB/minc\_insertion.pl (exit codes from 230 to 259)
+12\. Exit codes from uploadNeuroDB/minc\_insertion.pl (exit codes from 220 to 239)
 
 13\. Exit codes from uploadNeuroDB/register\_processed\_data.pl (exit codes from
-260 to 279)
+240 to 259)
 
-14\. Exit codes from uploadNeuroDB/tarchiveLoader (exit codes from 280 to 299)
+14\. Exit codes from uploadNeuroDB/tarchiveLoader (exit codes from 260 to 279)
 
 # LICENSING
 
--- uploadNeuroDB/NeuroDB/ExitCodes.pm
+++ uploadNeuroDB/NeuroDB/ExitCodes.pm
@@ -42,36 +42,36 @@ Below is a list of the possible exit codes organized per script:
 1. Common exit codes to most insertion scripts (exit codes from 0 to 19, 0 =
 exit script with success status)
 
-2. Exit codes from batch_uploads_imageuploader (exit codes from 20 to 49)
+2. Exit codes from batch_uploads_imageuploader (exit codes from 20 to 39)
 
 3. Exit codes from batch_uploads_tarchive (no exit codes available yet, exit
-codes will be from 60 to 69)
+codes will be from 40 to 59)
 
-4. Exit codes from dicom-archive/dicomTar.pl (exit codes from 80 to 89)
+4. Exit codes from dicom-archive/dicomTar.pl (exit codes from 60 to 79)
 
-5. Exit codes from dicom-archive/updateMRI_upload (exit codes from 100 to 119)
+5. Exit codes from dicom-archive/updateMRI_upload (exit codes from 80 to 99)
 
-6. Exit codes from DTIPrep/DTIPrep_pipeline.pl (exit codes from 120 to 139)
+6. Exit codes from DTIPrep/DTIPrep_pipeline.pl (exit codes from 100 to 119)
 
-7. Exit codes from DTIPrep/DTIPrepRegister.pl (exit codes from 140 to 159)
+7. Exit codes from DTIPrep/DTIPrepRegister.pl (exit codes from 120 to 139)
 
-8. Exit codes from uploadNeuroDB/imaging_upload_file.pl (exit codes from 160
+8. Exit codes from uploadNeuroDB/imaging_upload_file.pl (exit codes from 140
+to 159)
+
+9. Exit codes from uploadNeuroDB/NeuroDB/ImagingUpload.pm (exit codes from 160
 to 179)
 
-9. Exit codes from uploadNeuroDB/NeuroDB/ImagingUpload.pm (exit codes from 180
-to 199)
-
 10. Exit codes from uploadNeuroDB/NeuroDB/MRIProcessingUtility.pm (exit codes
- from 200 to 219)
+ from 180 to 199)
 
-11. Exit codes from uploadNeuroDB/minc_deletion.pl (exit codes from 220 to 239)
+11. Exit codes from uploadNeuroDB/minc_deletion.pl (exit codes from 200 to 219)
 
-12. Exit codes from uploadNeuroDB/minc_insertion.pl (exit codes from 230 to 259)
+12. Exit codes from uploadNeuroDB/minc_insertion.pl (exit codes from 220 to 239)
 
 13. Exit codes from uploadNeuroDB/register_processed_data.pl (exit codes from
-260 to 279)
+240 to 259)
 
-14. Exit codes from uploadNeuroDB/tarchiveLoader (exit codes from 280 to 299)
+14. Exit codes from uploadNeuroDB/tarchiveLoader (exit codes from 260 to 279)
 
 
 =head1 LICENSING
@@ -131,7 +131,7 @@ our $PNAME_PHANTOM_MISMATCH     = 23; # if patient name provided in the text
 
 #### --- FROM batch_uploads_tarchive
 
-#TODO: TO BE DONE LATER ON (ERROR CODES WOULD BE BETWEEN 20 & 29)
+#TODO: TO BE DONE LATER ON (ERROR CODES WOULD BE BETWEEN 40 & 59)
 
 
 
@@ -141,14 +141,14 @@ our $PNAME_PHANTOM_MISMATCH     = 23; # if patient name provided in the text
 #### --- FROM dicom-archive/dicomTar.pl
 
 # input error checking and setting failures
-our $TARGET_EXISTS_NO_CLOBBER  = 40; # if tarchive already exists but option
+our $TARGET_EXISTS_NO_CLOBBER  = 60; # if tarchive already exists but option
                                      # -clobber was not set
 
 # database related failures
-our $TARCHIVE_INSERT_FAILURE   = 41; # if insertion in tarchive tables failed
+our $TARCHIVE_INSERT_FAILURE   = 61; # if insertion in tarchive tables failed
 
 # script execution failures
-our $UPDATE_MRI_UPLOAD_FAILURE = 42; # if updateMRI_Upload.pl execution failed
+our $UPDATE_MRI_UPLOAD_FAILURE = 62; # if updateMRI_Upload.pl execution failed
 
 
 
@@ -158,7 +158,7 @@ our $UPDATE_MRI_UPLOAD_FAILURE = 42; # if updateMRI_Upload.pl execution failed
 #### --- FROM dicom-archive/updateMRI_upload.pl
 
 # validation failures
-our $TARCHIVE_ALREADY_UPLOADED = 60; # if the tarchive was already uploaded
+our $TARCHIVE_ALREADY_UPLOADED = 80; # if the tarchive was already uploaded
 
 
 
@@ -168,13 +168,14 @@ our $TARCHIVE_ALREADY_UPLOADED = 60; # if the tarchive was already uploaded
 #### --- FROM DTIPrep/DTIPrep_pipeline.pl
 
 # validation failures
-our $NO_DTIPREP_VERSION       = 80; # if no DTIPrep version could be found
-our $NO_MINCDIFFUSION_VERSION = 81; # if no mincdiffusion version could be found
+our $NO_DTIPREP_VERSION       = 100; # if no DTIPrep version could be found
+our $NO_MINCDIFFUSION_VERSION = 101; # if no mincdiffusion version could be
+                                     # found
                                     # NOTE: ALSO USED BY DTIPrepRegister.pl
-our $NO_NIAK_PATH             = 82; # if no valid NIAK path could be found
+our $NO_NIAK_PATH             = 102; # if no valid NIAK path could be found
 
 # processing exits
-our $NO_POST_PROCESSING_TO_RUN = 83; # if no post-processing will be run
+our $NO_POST_PROCESSING_TO_RUN = 103; # if no post-processing will be run
 
 
 
@@ -184,13 +185,13 @@ our $NO_POST_PROCESSING_TO_RUN = 83; # if no post-processing will be run
 #### --- FROM DTIPrep/DTIPrepRegister.pl
 
 # validation failures
-our $UNREADABLE_DTIPREP_PROTOCOL = 100; # if DTIPrep XML protocol cannot be
+our $UNREADABLE_DTIPREP_PROTOCOL = 120; # if DTIPrep XML protocol cannot be
 # read
-our $GET_OUTPUT_LIST_FAILURE     = 101; # if could not get the list of outputs
+our $GET_OUTPUT_LIST_FAILURE     = 121; # if could not get the list of outputs
                                         # for the DTI file
-our $MISSING_PREPROCESSED_FILES  = 102; # if some preprocess files are missing
-our $MISSING_POSTPROCESSED_FILES = 103; # if some post-process files are missing
-our $NO_TOOL_NAME_VERSION        = 104; # if tool name & version not available
+our $MISSING_PREPROCESSED_FILES  = 122; # if some preprocess files are missing
+our $MISSING_POSTPROCESSED_FILES = 123; # if some post-process files are missing
+our $NO_TOOL_NAME_VERSION        = 124; # if tool name & version not available
 
 
 
@@ -199,21 +200,21 @@ our $NO_TOOL_NAME_VERSION        = 104; # if tool name & version not available
 #### --- FROM uploadNeuroDB/imaging_upload_file.pl
 
 # input error checking and setting failures
-our $UPLOAD_ID_PATH_MISMATCH = 120; # if upload path given as an argument does
+our $UPLOAD_ID_PATH_MISMATCH = 140; # if upload path given as an argument does
                                     # not match the path stored in the
                                     # mri_upload table for the UploadID given
                                     # as an argument
-our $INVALID_DICOM_CAND_INFO = 121; # if files in tarchive are not all DICOMs
+our $INVALID_DICOM_CAND_INFO = 141; # if files in tarchive are not all DICOMs
                                     # or if at least one patient name mismatch
                                     # between the one stored in DICOM files and
                                     # the one stored in mri_upload
 
 # script execution failures
-our $DICOMTAR_FAILURE       = 122; # if dicomTar.pl execution failed
-our $TARCHIVELOADER_FAILURE = 123; # if tarchiveLoader execution failed
+our $DICOMTAR_FAILURE        = 142; # if dicomTar.pl execution failed
+our $TARCHIVELOADER_FAILURE  = 143; # if tarchiveLoader execution failed
 
-our $CLEANUP_UPLOAD_FAILURE = 124; # if removal/clean up of the uploaded file in
-                                   # the incoming folder failed
+our $CLEANUP_UPLOAD_FAILURE  = 144; # if removal/clean up of the uploaded
+                                    # file in the incoming folder failed
 
 
 
@@ -223,7 +224,7 @@ our $CLEANUP_UPLOAD_FAILURE = 124; # if removal/clean up of the uploaded file in
 #### --- FROM uploadNeuroDB/NeuroDB/ImagingUpload.pm
 
 # validation failures
-our $DICOM_PNAME_EXTRACTION_FAILURE = 140; # if tarchive was already uploaded
+our $DICOM_PNAME_EXTRACTION_FAILURE = 160; # if tarchive was already uploaded
 
 
 
@@ -233,20 +234,20 @@ our $DICOM_PNAME_EXTRACTION_FAILURE = 140; # if tarchive was already uploaded
 #### --- FROM uploadNeuroDB/NeuroDB/MRIProcessingUtility.pm
 
 # database related failures
-our $TARCHIVE_NOT_IN_DB        = 160; # if tarchive not found in the database
-our $GET_PSC_FAILURE           = 161; # if could not determine PSC from the DB
-our $GET_SCANNERID_FAILURE     = 162; # if could not determine scannerID from DB
-our $CAND_REGISTRATION_FAILURE = 163; # if candidate registration failed
+our $TARCHIVE_NOT_IN_DB        = 180; # if tarchive not found in the database
+our $GET_PSC_FAILURE           = 181; # if could not determine PSC from the DB
+our $GET_SCANNERID_FAILURE     = 182; # if could not determine scannerID from DB
+our $CAND_REGISTRATION_FAILURE = 183; # if candidate registration failed
 
 
 # file related failures
-our $EXTRACT_ARCHIVE_FAILURE = 164; # if extraction of the archive failed
-our $CORRUPTED_TARCHIVE      = 165; # if mismatch between md5sum stored in the
+our $EXTRACT_ARCHIVE_FAILURE = 184; # if extraction of the archive failed
+our $CORRUPTED_TARCHIVE      = 185; # if mismatch between md5sum stored in the
                                     # tarchive table and the md5sum of the
                                     # tarchive from the file system
 
 # study related failures
-our $GET_SUBJECT_ID_FAILURE = 166; # if the getSubjectIDs function from the
+our $GET_SUBJECT_ID_FAILURE = 186; # if the getSubjectIDs function from the
                                    # profile does not return subject IDs
 
 
@@ -257,7 +258,7 @@ our $GET_SUBJECT_ID_FAILURE = 166; # if the getSubjectIDs function from the
 #### --- FROM uploadNeuroDB/minc_deletion.pl
 
 # validation failures
-our $FILEID_SERIESUID_ARG_FAILURE = 180; # if seriesUID and fileID both provided
+our $FILEID_SERIESUID_ARG_FAILURE = 200; # if seriesUID and fileID both provided
                                          # as input to the file (it should
                                          # always be one or the other)
 
@@ -269,12 +270,12 @@ our $FILEID_SERIESUID_ARG_FAILURE = 180; # if seriesUID and fileID both provided
 #### --- FROM uploadNeuroDB/minc_insertion.pl
 
 # validation failures
-our $INVALID_TARCHIVE   = 200; # if tarchive validation is not set to 1 in the
+our $INVALID_TARCHIVE   = 220; # if tarchive validation is not set to 1 in the
                                # mri_upload table
-our $CANDIDATE_MISMATCH = 201; # if candidate PSCID and CandID do not match
-our $UNKNOW_PROTOCOL    = 202; # if could not find acquisition protocol of the
+our $CANDIDATE_MISMATCH = 221; # if candidate PSCID and CandID do not match
+our $UNKNOW_PROTOCOL    = 222; # if could not find acquisition protocol of the
                                # MINC
-our $PROTOCOL_NOT_IN_PROFILE = 203; # if the acquisition protocol could be
+our $PROTOCOL_NOT_IN_PROFILE = 223; # if the acquisition protocol could be
                                     # determined but is not included in the
                                     # isFileToBeRegisteredGivenProtocol function
                                     # of the profile file
@@ -287,14 +288,14 @@ our $PROTOCOL_NOT_IN_PROFILE = 203; # if the acquisition protocol could be
 #### --- FROM uploadNeuroDB/register_processed_data.pl
 
 # validation failures
-our $INVALID_SOURCEFILEID = 220; # if source file ID argument is not valid
+our $INVALID_SOURCEFILEID = 240; # if source file ID argument is not valid
 
 # database related failures
-our $GET_SESSIONID_FROM_SOURCEFILEID_FAILURE = 221; # if failed to get SessionID
+our $GET_SESSIONID_FROM_SOURCEFILEID_FAILURE = 241; # if failed to get SessionID
                                                     # from the sourceFileID
-our $GET_ACQUISITION_PROTOCOL_ID_FAILURE     = 222; # if failed to determine the
+our $GET_ACQUISITION_PROTOCOL_ID_FAILURE     = 242; # if failed to determine the
                                                     # acquisition protocol ID
-our $FILE_REGISTRATION_FAILURE               = 223; # if file registration
+our $FILE_REGISTRATION_FAILURE               = 243; # if file registration
                                                     # into the database failed
 
 
@@ -305,10 +306,10 @@ our $FILE_REGISTRATION_FAILURE               = 223; # if file registration
 #### --- FROM uploadNeuroDB/tarchiveLoader
 
 # script execution failures
-our $TARCHIVE_VALIDATION_FAILURE = 240; # if tarchive_validation.pl failed
+our $TARCHIVE_VALIDATION_FAILURE = 260; # if tarchive_validation.pl failed
 
 # file related failures
-our $NO_VALID_MINC_CREATED = 241; # if no valid MINC file was created
+our $NO_VALID_MINC_CREATED = 261; # if no valid MINC file was created
                                   # (non-localizers)
-our $NO_MINC_INSERTED      = 242; # if no MINC files was inserted (invalid
+our $NO_MINC_INSERTED      = 262; # if no MINC files was inserted (invalid
                                   # study)
