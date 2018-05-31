ef2840392d27af25bcbcd95e8d83857df47c7162
Cecile Madjar <cecile.madjar@gmail.com>
2018-05-31 16:05:04 -0400

    finished creating the script to remove entries in the database and in the filesystem for JIV

--- tools/remove_jiv_data_from_db_and_filesystem.pl
+++ tools/remove_jiv_data_from_db_and_filesystem.pl
@@ -30,9 +30,13 @@ some C<ArchiveLocation> paths stored from the root directory.
 
 use strict;
 use warnings;
+
 use Getopt::Tabular;
+use File::Path qw(remove_tree);
+
 use NeuroDB::DBI;
 use NeuroDB::ExitCodes;
+
 
 
 my $profile;
@@ -62,6 +66,9 @@ USAGE
 &Getopt::Tabular::SetHelp($Help, $Usage);
 &Getopt::Tabular::GetOptions(\@opt_table, \@ARGV) || exit 1;
 
+
+
+
 ## Input option error checking
 if ( !$profile ) {
     print $Help;
@@ -75,9 +82,15 @@ if ( !@Settings::db ) {
     exit $NeuroDB::ExitCodes::DB_SETTINGS_FAILURE;
 }
 
+
+
+
 ## establish database connection
 my $dbh  = &NeuroDB::DBI::connect_to_db(@Settings::db);
 print "\n==> Successfully connected to database \n";
+
+
+
 
 ## select the JIV parameter type ID from parameter_type
 my $query = "SELECT ParameterTypeID FROM parameter_type WHERE Name='jiv_path'";
@@ -87,9 +100,49 @@ $sth->execute();
 my $row = $sth->fetchrow_hashref();
 if ( !$row ) {
     print "\n==> did not find any entry in the parameter_type table with "
-          . "Name='jiv_path'. Exiting now.\n\n";
+          . "Name='jiv_path'. Exiting now.\n";
     exit $NeuroDB::ExitCodes::SUCCESS;
 }
 my $param_type_id = $row->{'ParameterTypeID'};
 
-print $param_type_id;
+
+
+
+## check to see if there are entries in the parameter_file table
+$query = "SELECT * FROM parameter_file WHERE ParameterTypeID=?";
+$sth   = $dbh->prepare($query);
+$sth->execute($param_type_id);
+$row        = $sth->fetchrow_hashref();
+my $message = "\n==> did not find any JIV entries in table parameter_file.\n";
+print $message if ( !$row );
+
+# delete entries from parameter_file
+my $delete     = "DELETE FROM parameter_file WHERE ParameterTypeID=?";
+my $delete_sth = $dbh->prepare($delete);
+$delete_sth->execute($param_type_id);
+
+# check to make sure entries have been deleted
+$sth->execute($param_type_id);
+$row     = $sth->fetchrow_hashref();
+if ( !$row ) {
+    print "\n==> succesfully deleted all JIV entries in parameter_file.\n";
+} else {
+    print "\n==> could not delete all JIV entries in parameter_file.\n";
+    exit;
+}
+
+
+
+
+## delete the JIV directory from the filesystem
+# grep the data_dir from the Configuration module of LORIS
+my $data_dir = &NeuroDB::DBI::getConfigSetting(\$dbh, 'dataDirBasepath');
+$data_dir    =~ s/\/$//;
+my $jiv_dir  = $data_dir . "/jiv";
+remove_tree($jiv_dir) if (-d $jiv_dir);
+
+
+
+
+
+exit $NeuroDB::ExitCodes::SUCCESS;
