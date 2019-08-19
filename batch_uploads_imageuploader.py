5c897e9209a4f66b42873558c8f9ec91662f117f
xlecours <xavier.lecoursboucher@mcgill.ca>
2019-08-19 10:39:44 -0400

    adding option descriptions

--- batch_uploads_imageuploader.py
+++ batch_uploads_imageuploader.py
@@ -38,9 +38,9 @@ autostart (-a) option when calling this script.
 """
 ))
 
-parser.add_argument('-b', '--baseurl', required=True)
-parser.add_argument('-i', '--inputfile', required=True)
-parser.add_argument('-a', '--autostart', action='store_const', const=True)
+parser.add_argument('-b', '--baseurl', required=True, help='LORIS API url with version.')
+parser.add_argument('-i', '--inputfile', required=True, help='The list of scans.')
+parser.add_argument('-a', '--autostart', action='store_const', const=True, help='If present, will attemp to start the mri_upload process for each scans.')
 
 args = parser.parse_args()
 
