f226185f4cbadd243c5bd798851cf1675b5a40d1
Cécile <cecile.madjar@gmail.com>
2020-01-22 14:04:36 -0500

    added a mass nifti pic creation script

--- python/mass_nifti_pic.py
+++ python/mass_nifti_pic.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-"""Script to mass chunk electrophysiology datasets."""
+"""Script to mass create the pic images of inserted NIfTI files."""
 
 import os
 import sys
 import getopt
 import lib.exitcode
-from lib.database      import Database
-from lib.physiological import Physiological
+from lib.database import Database
+from lib.imaging  import Imaging
 
 
 __license__ = "GPLv3"
@@ -31,13 +31,12 @@ def main():
 
     usage = (
         '\n'
-        'usage  : mass_electrophysiology_chunking.py -p <profile> -s <smallest_id> '
-                  '-l <largest_id>\n\n'
+        'usage  : mass_nifti_pic.py -p <profile> -s <smallest_id> -l <largest_id>\n\n'
         'options: \n'
         '\t-p, --profile    : name of the python database config file in '
                               'dicom-archive/.loris-mri\n'
-        '\t-s, --smallest_id: smallest PhyiologicalFileID to chunk\n'
-        '\t-l, --largest_id : largest PhysiologicalFileID to chunk\n'
+        '\t-s, --smallest_id: smallest FileID for which the pic will be created\n'
+        '\t-l, --largest_id : largest FileID for which the pic will be created\n'
         '\t-v, --verbose    : be verbose\n'
     )
 
@@ -63,13 +62,12 @@ def main():
     # input error checking and load config_file file
     config_file = input_error_checking(profile, smallest_id, largest_id, usage)
 
-    # run chunking script on electrophysiology datasets with a PhysiologicalFileID
-    # between smallest_id and largest_id
+    # create pic for NIfTI files with a FileID between smallest_id and largest_id
     if (smallest_id == largest_id):
-        make_chunks(smallest_id, config_file, verbose)
+        make_pic(smallest_id, config_file, verbose)
     else:
         for file_id in range(smallest_id, largest_id):
-            make_chunks(file_id, config_file, verbose)
+            make_pic(file_id, config_file, verbose)
 
 
 def input_error_checking(profile, smallest_id, largest_id, usage):
@@ -80,9 +78,9 @@ def input_error_checking(profile, smallest_id, largest_id, usage):
 
     :param profile    : path to the profile file with MySQL credentials
      :type profile    : str
-    :param smallest_id: smallest PhysiologicalFileID on which to run the chunking script
+    :param smallest_id: smallest FileID for which to create the pic
      :type smallest_id: int
-    :param largest_id : largest PhysiologicalFileID on which to run the chunking script
+    :param largest_id : largest FileID for which to create the pic
      :type largest_id : int
     :param usage      : script usage to be displayed when encountering an error
      :type usage      : str
@@ -132,13 +130,13 @@ def input_error_checking(profile, smallest_id, largest_id, usage):
     return config_file
 
 
-def make_chunks(physiological_file_id, config_file, verbose):
+def make_pic(file_id, config_file, verbose):
     """
-    Call the function create_chunks_for_visualization of the Physiology class on
-    the PhysiologicalFileID provided as argument to this function.
+    Call the function create_imaging_pic of the Imaging class on
+    the FileID provided as argument to this function.
 
-    :param physiological_file_id: PhysiologicalFileID of the file to chunk
-     :type physiological_file_id: int
+    :param file_id    : FileID of the file for which to create the pic
+     :type file_id    : int
     :param config_file: path to the config file with database connection information
      :type config_file: str
     :param verbose    : flag for more printing if set
@@ -155,13 +153,46 @@ def make_chunks(physiological_file_id, config_file, verbose):
     # making sure that there is a final / in data_dir
     data_dir = data_dir if data_dir.endswith('/') else data_dir + "/"
 
-    # load the Physiological object
-    physiological = Physiological(db, verbose)
+    # load the Imaging object
+    imaging = Imaging(db, verbose)
 
-    # create the chunked dataset
-    if physiological.grep_file_path_from_file_id(physiological_file_id):
-        print('Chunking physiological file ID ' + physiological_file_id)
-        physiological.create_chunks_for_visualization(physiological_file_id, data_dir)
+    # grep the NIfTI file path
+    nii_file_path = imaging.grep_file_path_from_file_id(file_id)
+    if not nii_file_path:
+        print('WARNING: no file in the database with FileID = ' + file_id)
+        return
+    if not os.path.exists(data_dir + nii_file_path):
+        print('WARNING: file ' + nii_file_path + ' not found on the filesystem')
+        return
+
+    # grep the time length from the NIfTI file header
+    is_4d_dataset = False
+    length_parameters = imaging.get_nifti_image_length_parameters(data_dir + nii_file_path)
+    if len(length_parameters) == 4:
+        file_parameters['time'] = length_parameters[3]
+        is_4d_dataset = True
+
+    # grep the SessionID of the file
+    cand_id = imaging.grep_cand_id_from_file_id(file_id)
+    if not cand_id:
+        print('WARNING: CandID not found for FileID ' + file_id)
+
+    # create the pic
+    pic_rel_path = imaging.create_imaging_pic(
+        {
+            'cand_id'      : cand_id,
+            'data_dir_path': data_dir,
+            'file_rel_path': nii_file_path,
+            'is_4D_dataset': is_4d_dataset,
+            'file_id'      : file_id
+        }
+    )
+    if not os.path.exists(data_dir + 'pic/' + pic_rel_path):
+        print('WARNING: the pic ' + data_dir + 'pic/' + pic_rel_path + 'was not created')
+        return
+
+    # insert the relative path to the pic in the parameter_file table
+    imaging.insert_parameter_file(file_id, 'check_pic_filename', pic_rel_path)
 
 
 if __name__ == "__main__":
