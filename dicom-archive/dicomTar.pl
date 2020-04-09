012c1fde7a8cf9a4f4edda1b2b1563d6d86229cd
nicolasbrossard <nicolasbrossard.mni@gmail.com>
2020-04-09 09:56:27 -0400

    Fixed invalid find command in dicomTar.pl. (#540)

--- dicom-archive/dicomTar.pl
+++ dicom-archive/dicomTar.pl
@@ -201,7 +201,7 @@ my $system          = `uname`;
 
 # Remove all files starting with . and __MACOSX in the dcm_source directory
 my @args = ($dcm_source);
-push(@args, qw/-type -f -name __MACOSX -delete -o -name .* -delete/);
+push(@args, qw/-type f -name __MACOSX -delete -o -name .* -delete/);
 system('find', @args);
 
 # create new summary object
