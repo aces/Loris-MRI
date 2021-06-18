a521039f1721c7ead639081f4b6a1fe4dfd9c038
cmadjar <cecile.madjar@gmail.com>
2021-06-18 16:51:21 -0400

    update documentation for dicom_validation_pipeline.py

--- python/lib/dcm2bids_imaging_pipeline_lib/dicom_validation_pipeline.py
+++ python/lib/dcm2bids_imaging_pipeline_lib/dicom_validation_pipeline.py
@@ -7,8 +7,22 @@ __license__ = "GPLv3"
 
 
 class DicomValidationPipeline(BasePipeline):
+    """
+    Pipeline that extends the BasePipeline class to add some specific DICOM validations to be
+    run to validate the DICOM archive processed.
+
+    Functions that starts with _ are functions specific to the DicomValidationPipeline class.
+    """
 
     def __init__(self, loris_getopt_obj, script_name):
+        """
+        Initiate the DicomValidationPipeline class and runs the different validations that need to
+        be run on the DICOM archive. The mri_upload table will be updated according to the finding
+        of the DICOM archive validation.
+
+        :param loris_getopt_obj: the LorisGetOpt object with getopt values provided to the pipeline
+         :type loris_getopt_obj: LorisGetOpt obj
+        """
         super().__init__(loris_getopt_obj, script_name)
         self.validate_subject_ids()
         self._validate_dicom_archive_md5sum()
@@ -25,6 +39,10 @@ class DicomValidationPipeline(BasePipeline):
         )
 
     def _validate_dicom_archive_md5sum(self):
+        """
+        Validates that the DICOM archive stored on the file system has the same md5sum than the one
+        logged in the <tarchive> table.
+        """
 
         self.log_info(message="Verifying DICOM archive md5sum (checksum)", is_error="N", is_verbose="Y")
 
