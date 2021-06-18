64e993f2e3a5c8d536f92180caff0275f1234191
cmadjar <cecile.madjar@gmail.com>
2021-06-18 11:57:59 -0400

    add the dicom_validation_pipeline class to git

---
+++ python/lib/dcm2bids_imaging_pipeline_lib/dicom_validation_pipeline.py
@@ -0,0 +1,40 @@
+import os
+
+import lib.exitcode
+from lib.dcm2bids_imaging_pipeline_lib.base_pipeline import BasePipeline
+
+
+class DicomValidationPipeline(BasePipeline):
+
+    def __init__(self, loris_getopt_obj, script_name):
+        super().__init__(loris_getopt_obj, script_name)
+        self.validate_subject_ids()
+
+        # ---------------------------------------------------------------------------------------------
+        # If we get here, the tarchive is validated & the script stops running so update mri_upload
+        # ---------------------------------------------------------------------------------------------
+        message = f"DICOM archive {self.options_dict['tarchive_path']['value']} is valid!"
+        self.log_info(message, is_error="N", is_verbose="Y")
+        self.mri_upload_db_obj.update_mri_upload(
+            upload_id=self.upload_id,
+            fields=("isTarchiveValidated", "Inserting",),
+            values=("1", "0")
+        )
+
+    def validate_dicom_archive_md5sum(self):
+
+        self.log_info(message="Verifying DICOM archive md5sum (checksum)", is_error="N", is_verbose="Y")
+
+        tarchive_path = os.path.join(self.data_dir, self.tarchive_db_obj.tarchive_info_dict["ArchiveLocation"])
+        result = self.tarchive_db_obj.validate_dicom_archive_md5sum(tarchive_path)
+        message = result["message"]
+
+        if result['success']:
+            self.log_info(message, is_error="N", is_verbose="Y")
+        else:
+            self.mri_upload_db_obj.update_mri_upload(
+                upload_id=self.upload_id,
+                fields=("isTarchiveValidated", "IsCandidateInfoValidated"),
+                values=("0", "0")
+            )
+            self.log_error_and_exit(message, lib.exitcode.CORRUPTED_FILE, is_error="Y", is_verbose="N")
