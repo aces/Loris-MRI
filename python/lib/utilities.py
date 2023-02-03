2c544160862ce8a85deb30b9b9f38352261c0c5a
jeffersoncasimir <jefferson.casimir@mcin.ca>
2023-02-03 12:14:47 -0500

    Added workaround for HDF5 files

--- python/lib/utilities.py
+++ python/lib/utilities.py
@@ -173,22 +173,25 @@ def update_set_file_path_info(set_file, fdt_file):
     # grep the basename without the extension of set_file
     basename = os.path.splitext(os.path.basename(set_file))[0]
 
-    # read the .set EEG file using scipy
-    dataset = scipy.io.loadmat(set_file)
+    try:
+        # read the .set EEG file using scipy
+        dataset = scipy.io.loadmat(set_file)
 
-    # update the EEG paths in the .set file
-    if 'filename' in dataset.keys():
-        dataset['filename'] = numpy.array(basename + ".set")
-    if 'setname' in dataset.keys():
-        dataset['setname'] = numpy.array(basename)
-    if 'EEG' in dataset.keys():
-        dataset['EEG'][0][0][1] = numpy.array(basename + ".set")
-    if fdt_file and 'EEG' in dataset.keys():
-        dataset['EEG'][0][0][15] = numpy.array(basename + ".fdt")
-        dataset['EEG'][0][0][40] = numpy.array(basename + ".fdt")
+        # update the EEG paths in the .set file
+        if 'filename' in dataset.keys():
+            dataset['filename'] = numpy.array(basename + ".set")
+        if 'setname' in dataset.keys():
+            dataset['setname'] = numpy.array(basename)
+        if 'EEG' in dataset.keys():
+            dataset['EEG'][0][0][1] = numpy.array(basename + ".set")
+        if fdt_file and 'EEG' in dataset.keys():
+            dataset['EEG'][0][0][15] = numpy.array(basename + ".fdt")
+            dataset['EEG'][0][0][40] = numpy.array(basename + ".fdt")
 
-    # write the new .set file with the correct path info
-    scipy.io.savemat(set_file, dataset, False)
+        # write the new .set file with the correct path info
+        scipy.io.savemat(set_file, dataset, False)
+    except NotImplementedError:     # Thrown for matlab v7.3 files
+        pass    # Assumes path fields are already set
 
 
 def compute_md5sum(file):
