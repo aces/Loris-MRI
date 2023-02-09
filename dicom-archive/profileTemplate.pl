9ca80f0e3562e5298e2d857f47376d872585a48e
regisoc <regis.ongaro-carcy@mcin.ca>
2023-02-08 19:51:08 -0500

    profil template

--- dicom-archive/profileTemplate.pl
+++ dicom-archive/profileTemplate.pl
@@ -12,7 +12,7 @@
 =pod
 # SECTION I
 # -----------------------------------------------------------------------------------------------
-  DATABASE settings 
+  DATABASE settings
   for database instance you are connecting to
   required: db_name, $db_user, $db_pass, $db_host
 # -----------------------------------------------------------------------------------------------
@@ -30,19 +30,19 @@
 # -----------------------------------------------------------------------------------------------
 =cut
 
-# extracts the subject and timepoint identifiers from the patient name 
+# extracts the subject and timepoint identifiers from the patient name
 # assumes identifers are stored as <PSCID>_<DCCID>_<visit> in PatientName field, where <visit> is 3 digits.
 sub getSubjectIDs {
     my ($patientName, $patientID, $scannerID, $dbhr, $db) = @_;
 
     my %subjectID; # Will stored subject IDs.
-    
+
      # If patientName is phantom scan or test scan
      # CandID is scanner DCCID (based on site alias)
      # visitLabel is scan patient name
-     # Set createVisitLable to 
-     #      a. 1 if imaging pipeline should create the visit label (when visit label has not been created yet in the database. 
-     #      b. 0 if imaging pipeline should not create the visit label (when visit label has not been created yet in the database. 
+     # Set createVisitLable to
+     #      a. 1 if imaging pipeline should create the visit label (when visit label has not been created yet in the database.
+     #      b. 0 if imaging pipeline should not create the visit label (when visit label has not been created yet in the database.
     if ($patientName =~ /PHA/i or $patientName =~ /TEST/i) {
 
         $subjectID{'CandID'}     = NeuroDB::MRI::my_trim(NeuroDB::MRI::getScannerCandID($scannerID, $db));
@@ -51,12 +51,12 @@ sub getSubjectIDs {
 
         $subjectID{'createVisitLabel'} = 1;
 
-        # When createVisitLabel is set to 1, SubprojectID must also
-        # be set to the ID of the subproject that the newly created
+        # When createVisitLabel is set to 1, CohortID must also
+        # be set to the ID of the cohort that the newly created
         # visit should have. Assuming for example that all patient
-        # names end with "_<mySubProjectID>", then we could write:
-        # ($subjectID{'SubprojectID'}) = $patientName =~ /_(\d+)$/;
-        # When createVisitLabel is set to 0, $subjectID{'SubprojectID'} is ignored.
+        # names end with "_<myCohortID>", then we could write:
+        # ($subjectID{'CohortID'}) = $patientName =~ /_(\d+)$/;
+        # When createVisitLabel is set to 0, $subjectID{'CohortID'} is ignored.
 
         # If config setting 'createVisitLabel' is true
         # then $subjectID{'ProjectID'} must be set to the project ID of the
@@ -64,7 +64,7 @@ sub getSubjectIDs {
         # names that contain the string 'HOSPITAL' are associated to visit
         # done for project with ID 1 and all others to projects with ID 2, we
         # could write:
-        # $subjectID{'ProjectID'} = $patientName =~ /HOSPITAL/  
+        # $subjectID{'ProjectID'} = $patientName =~ /HOSPITAL/
         #     ? 1 : 2;
         # When createVisitLabel is set to 0, $subjectID{'ProjectID'} is ignored.
 
@@ -78,16 +78,16 @@ sub getSubjectIDs {
         $subjectID{'isPhantom'}  = 0;
 
         $subjectID{'createVisitLabel'} = 0;
-  
-        # When createVisitLabel is set to 1, SubprojectID must also
-        # be set to the ID of the subproject that the newly created
+
+        # When createVisitLabel is set to 1, CohortID must also
+        # be set to the ID of the cohort that the newly created
         # visit should have. Assuming for example that visits V01 and V02
         # are associated with sub-project with ID 1 and all others to sub-project
         # with ID 2, then we could write:
-        # ($subjectID{'SubprojectID'}) = $subjectID{'visitLabel'} =~ /^V0[12]$/ 
+        # ($subjectID{'CohortID'}) = $subjectID{'visitLabel'} =~ /^V0[12]$/
         #     ? 1 : 2;
-        # When createVisitLabel is set to 0, $subjectID{'SubprojectID'} is ignored.
-        
+        # When createVisitLabel is set to 0, $subjectID{'CohortID'} is ignored.
+
         # If config setting 'createVisitLabel' is true
         # then $subjectID{'ProjectID'} must be set to the project ID of the
         # newly created visit. Assuming for example that candidates with a
@@ -98,11 +98,11 @@ sub getSubjectIDs {
         #     ? 1 : 2;
         # When createVisitLabel is set to 0, $subjectID{'ProjectID'} is ignored.
 
-        print "PSCID is: "            . $subjectID{'PSCID'}      . 
+        print "PSCID is: "            . $subjectID{'PSCID'}      .
                 "\n CandID id: "      . $subjectID{'CandID'}     .
                 "\n visit_label is: " . $subjectID{'visitLabel'} . "\n";
     }
-   
+
     # Return subjectIDs
     return \%subjectID;
 }
@@ -113,8 +113,8 @@ sub getSubjectIDs {
 # that meet an mri_protocol table entry to be inserted;
 # this is achieved by returning 1 when $acquisitionProtocol ne 'unknown'.
 # Alternatively, this routine can be tailored to the user's needs; it can be made to
-# insert scans based on exact (eq 't1') or partial regex matching (=~ /despot/), or case 
-# insensitive partial matching to the scan type (=~ /dti/i), etc... 
+# insert scans based on exact (eq 't1') or partial regex matching (=~ /despot/), or case
+# insensitive partial matching to the scan type (=~ /dti/i), etc...
 # as shown in the commented-out line below.
 sub isFileToBeRegisteredGivenProtocol {
     my $acquisitionProtocol = shift;
@@ -130,7 +130,7 @@ sub isFileToBeRegisteredGivenProtocol {
 sub  get_DTI_CandID_Visit {
     my ($native_dir) =   @_;
 
-    if  ($native_dir =~  /assembly\/(\d\d\d\d\d\d)\/(V\d{1,2})\/mri\//i)  {  
+    if  ($native_dir =~  /assembly\/(\d\d\d\d\d\d)\/(V\d{1,2})\/mri\//i)  {
         my  $subjID =   $1;
         my  $visit  =   $2;
         return  ($subjID,$visit);
