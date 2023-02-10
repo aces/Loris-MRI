4cac2b82f06e050a14949e9a90cb1190f109f0a5
cmadjar <cecile.madjar@gmail.com>
2023-02-10 13:04:43 -0500

    fix bug

--- python/lib/dcm2bids_imaging_pipeline_lib/nifti_insertion_pipeline.py
+++ python/lib/dcm2bids_imaging_pipeline_lib/nifti_insertion_pipeline.py
@@ -354,7 +354,7 @@ class NiftiInsertionPipeline(BasePipeline):
             scan_type_id = self.imaging_obj.get_scan_type_id_from_scan_type_name(self.loris_scan_type)
             for protocol_info in protocols_list:
                 if protocol_info['Scan_type'] == scan_type_id:
-                    return protocol_info['scan_type_id'], protocol_info['mri_protocol_group_id']
+                    return scan_type_id, protocol_info['mri_protocol_group_id']
 
         protocol_info = self.imaging_obj.get_acquisition_protocol_info(
             protocols_list, nifti_name, scan_param, scan_type
