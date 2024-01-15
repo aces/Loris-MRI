5d2aa6d5821f771a64bf58116dd6861d1f36fd51
breen0074 <105391285+breen0074@users.noreply.github.com>
2024-01-15 16:41:17 -0600

    Fixes for parameter file value lists (#1025)
    * rm conversion to str on value list
    
    * add check to ensure list is not empty

--- python/lib/imaging.py
+++ python/lib/imaging.py
@@ -169,9 +169,9 @@ class Imaging:
 
         # convert list values into strings that could be inserted into parameter_file
         if type(value) == list:
-            if type(value[0]) in [float, int]:
+            if value and type(value[0]) in [float, int]:
                 value = [str(f) for f in value]
-            value = f"[{', '.join(str(value))}]"
+            value = f"[{', '.join(value)}]"
 
         # Gather column name & values to insert into parameter_file
         param_type_id = self.get_parameter_type_id(parameter_name)
