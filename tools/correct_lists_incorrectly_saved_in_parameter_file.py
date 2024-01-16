998239dcbd727cbb062ca218d95cf3ace41c9ef8
cmadjar <cecile.madjar@gmail.com>
2024-01-16 11:42:52 -0500

    prints

--- tools/correct_lists_incorrectly_saved_in_parameter_file.py
+++ tools/correct_lists_incorrectly_saved_in_parameter_file.py
@@ -63,6 +63,7 @@ def get_list_of_entries_in_parameter_file_to_correct(db):
         new_value_str = value_str.replace("[[, ', ", "[").replace(", ', ]]", "]").replace(", ", "")
         new_value_str = new_value_str.replace("'", "").replace(" ", '').replace(",", ", ")
         if new_value_str == "[[FalseFalseFalse]]":
+            print('in')
             new_value_str = "[False, False, False]"
 
         db.update('UPDATE parameter_file SET Value=%s WHERE ParameterFileID=%s', (new_value_str, param_file_id))
