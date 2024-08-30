eaa5b2d7631b1e9fafa3bc03d9b84fd941e328e4
kongtiaowang <kongtiaowangshen@gmail.com>
2024-08-30 15:10:31 -0400

    test

--- test/Dockerfile.test.py
+++ test/Dockerfile.test.py
@@ -1,10 +1,18 @@
 # Dockerfile
 FROM python:3.11.3-slim
 
+
 WORKDIR /app
 
 # Copy test files into the container
 COPY test /app/test
+
+RUN echo "Working directory:"
+RUN pwd
+RUN echo "Directory contents:"
+RUN ls -R
+
+
 
 # Install pytest
 RUN pip install pytest
