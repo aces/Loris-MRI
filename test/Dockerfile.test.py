1f59ac8aa870a2736240c2a92320a41d34da5625
kongtiaowang <kongtiaowangshen@gmail.com>
2024-08-30 13:03:18 -0400

    test

---
+++ test/Dockerfile.test.py
@@ -0,0 +1,14 @@
+# Dockerfile
+FROM python:3.11.3-slim
+
+WORKDIR /app
+
+# Copy test files into the container
+COPY test /app/test
+
+# Install pytest
+RUN pip install pytest
+
+# Set the default command to run tests
+CMD ["pytest", "/app/test"]
+
