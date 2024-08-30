8f75670e557260417d5e41e849715fcba11fed2b
kongtiaowang <kongtiaowangshen@gmail.com>
2024-08-30 14:54:43 -0400

    test

--- test/Dockerfile.test.py
+++ test/Dockerfile.test.py
@@ -3,12 +3,9 @@ FROM python:3.11.3-slim
 
 WORKDIR /app
 
-# Copy test files into the container
-COPY test /app/test
-
 # Install pytest
 RUN pip install pytest
 
 # Set the default command to run tests
-CMD ["pytest", "/app/test"]
+CMD ["pytest"]
 
