e238b40311315201c257cea2a7208bb670d342f7
Maxime Mulder <maxime-mulder@outlook.com>
2024-10-10 12:31:47 -0400

    remove mysql-connector

--- python/lib/db/connect.py
+++ python/lib/db/connect.py
@@ -1,4 +1,5 @@
 from typing import Any
+from urllib.parse import quote
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -9,9 +10,9 @@ default_port = 3306
 def connect_to_db(credentials: dict[str, Any]):
     host     = credentials['host']
     port     = credentials['port']
-    username = credentials['username']
-    password = credentials['passwd']
+    username = quote(credentials['username'])
+    password = quote(credentials['passwd'])
     database = credentials['database']
     port     = int(port) if port else default_port
-    engine = create_engine(f'mariadb+mysqlconnector://{username}:{password}@{host}:{port}/{database}')
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@{host}:{port}/{database}')
     return Session(engine)
