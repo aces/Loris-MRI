e91efc1b330f5f87ecd0658cc753f712cfd64a98
Maxime Mulder <maxime-mulder@outlook.com>
2024-10-20 18:02:21 -0400

    add defaults check

--- python/lib/db/base.py
+++ python/lib/db/base.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase, MappedAsDataclass):
     """
     Base SQLAlchemy class that must be inherited by all the ORM model classes.
     """
