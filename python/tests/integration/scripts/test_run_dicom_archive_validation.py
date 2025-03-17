8eedc1a9e4fcc5aa72526f70e9f766ad90579253
cmadjar <cecile.madjar@gmail.com>
2025-03-17 17:15:01 -0400

    refactor

--- python/tests/integration/scripts/test_run_dicom_archive_validation.py
+++ python/tests/integration/scripts/test_run_dicom_archive_validation.py
@@ -17,24 +17,6 @@ def reset_mri_upload_before_running(db: Database):
     mri_upload.number_of_minc_inserted = None
 
 
-def check_error_code_and_process_errors(
-        process: any,
-        expected_error_code: int,
-        expected_error_message: str
-):
-    # Print the standard output and error for debugging
-    print(f'STDOUT:\n{process.stdout.decode()}')
-    print(f'STDERR:\n{process.stderr.decoed()}')
-
-    # Isolate STDOUT message and check that it contains the expected error message
-    error_msg_is_valid = True if expected_error_message in process.stdout.decode() else False
-    assert error_msg_is_valid is True
-
-    # Check that the return code and standard error are correct
-    assert process.returncode == expected_error_code
-    assert process.stderr == b''
-
-
 def test_missing_upload_id_arg():
     db = get_integration_database_session()
 
@@ -48,12 +30,19 @@ def test_missing_upload_id_arg():
         '--tarchive_path', '/data/loris/tarchive/DCM_2015-07-07_ImagingUpload-14-30-FoTt1K.tar',
     ], capture_output=True)
 
-    # Check error code, error message returned and STDERR - will also print out STDERR and STDOUT
-    check_error_code_and_process_errors(
-        process,
-        expected_error_code=MISSING_ARG,
-        expected_error_message="[ERROR   ] argument --upload_id is required"
-    )
+    # Print the standard output and error for debugging
+    print(f'STDOUT:\n{process.stdout.decode()}')
+    print(f'STDERR:\n{process.stderr.decode()}')
+
+    # Isolate STDOUT message and check that it contains the expected error message
+    error_msg_is_valid = True \
+        if "[ERROR   ] argument --upload_id is required" in process.stdout.decode() \
+        else False
+    assert error_msg_is_valid is True
+
+    # Check that the return code and standard error are correct
+    assert process.returncode == MISSING_ARG
+    assert process.stderr == b''
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'MTL001_300001_V2')
@@ -76,12 +65,19 @@ def test_missing_tarchive_path_arg():
         '--upload_id', 126,
     ], capture_output=True)
 
-    # Check error code, error message returned and STDERR - will also print out STDERR and STDOUT
-    check_error_code_and_process_errors(
-        process,
-        expected_error_code=MISSING_ARG,
-        expected_error_message="[ERROR   ] argument --tarchive_path is required"
-    )
+    # Print the standard output and error for debugging
+    print(f'STDOUT:\n{process.stdout.decode()}')
+    print(f'STDERR:\n{process.stderr.decode()}')
+
+    # Isolate STDOUT message and check that it contains the expected error message
+    error_msg_is_valid = True \
+        if "[ERROR   ] argument --tarchive_path is required" in process.stdout.decode() \
+        else False
+    assert error_msg_is_valid is True
+
+    # Check that the return code and standard error are correct
+    assert process.returncode == MISSING_ARG
+    assert process.stderr == b''
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'MTL001_300001_V2')
