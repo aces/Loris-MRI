e5c0687a1bf97f20623796f4c9e719eded61cbd1
cmadjar <cecile.madjar@gmail.com>
2025-03-18 14:32:25 -0400

    add test to check invalid arguments

--- python/tests/integration/scripts/test_run_dicom_archive_validation.py
+++ python/tests/integration/scripts/test_run_dicom_archive_validation.py
@@ -3,7 +3,7 @@ import subprocess
 from sqlalchemy.orm import Session as Database
 
 from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
-from lib.exitcode import MISSING_ARG
+from lib.exitcode import MISSING_ARG, INVALID_ARG
 from tests.util.database import get_integration_database_session
 
 
@@ -78,6 +78,40 @@ def test_missing_tarchive_path_arg():
 
     # Check that the return code and standard error are correct
     assert process.returncode == MISSING_ARG
+    assert process.stderr == b''
+
+    # Check that the expected data has been inserted in the database
+    mri_upload = get_mri_upload_with_patient_name(db, 'MTL001_300001_V2')
+    assert mri_upload.inserting is False
+    assert mri_upload.is_candidate_info_validated is False
+    assert mri_upload.is_dicom_archive_validated is False
+    assert mri_upload.session is None
+
+def test_invalid_arg():
+    db = get_integration_database_session()
+
+    # Set some tarchive fields
+    reset_mri_upload_before_running(db)
+
+    # Run the script to test
+    process = subprocess.run([
+        'run_dicom_archive_validation.py',
+        '--profile', 'database_config.py',
+        '--invalid_arg', '126',
+    ], capture_output=True)
+
+    # Print the standard output and error for debugging
+    print(f'STDOUT:\n{process.stdout.decode()}')
+    print(f'STDERR:\n{process.stderr.decode()}')
+
+    # Isolate STDOUT message and check that it contains the expected error message
+    error_msg_is_valid = True \
+        if "[ERROR   ] --invalid_arg is not a valid option for the script" in process.stdout.decode() \
+        else False
+    assert error_msg_is_valid is True
+
+    # Check that the return code and standard error are correct
+    assert process.returncode == INVALID_ARG
     assert process.stderr == b''
 
     # Check that the expected data has been inserted in the database
