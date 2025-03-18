91514a67de205a10d762508e2841d0295aac7c4c
cmadjar <cecile.madjar@gmail.com>
2025-03-18 15:19:20 -0400

    add test to check if uploadID exists

--- python/tests/integration/scripts/test_run_dicom_archive_validation.py
+++ python/tests/integration/scripts/test_run_dicom_archive_validation.py
@@ -3,7 +3,7 @@ import subprocess
 from sqlalchemy.orm import Session as Database
 
 from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
-from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, MISSING_ARG
+from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE
 from tests.util.database import get_integration_database_session
 
 
@@ -157,3 +157,31 @@ def test_invalid_tarchive_path_arg():
     assert mri_upload.is_candidate_info_validated is False
     assert mri_upload.is_dicom_archive_validated is False
     assert mri_upload.session is None
+
+
+def test_non_existent_upload_id():
+    db = get_integration_database_session()
+
+    # Set some tarchive fields
+    reset_mri_upload_before_running(db)
+
+    # Run the script to test
+    process = subprocess.run([
+        'run_dicom_archive_validation.py',
+        '--profile', 'database_config.py',
+        '--tarchive_path', '/data/loris/tarchive/DCM_2015-07-07_ImagingUpload-14-30-FoTt1K.tar',
+        '--upload_id', '16666',
+    ], capture_output=True)
+
+    # Print the standard output and error for debugging
+    print(f'STDOUT:\n{process.stdout.decode()}')
+    print(f'STDERR:\n{process.stderr.decode()}')
+
+    # Isolate STDOUT message and check that it contains the expected error message
+    error_msg = "[ERROR   ] Did not find an entry in mri_upload associated with 'UploadID' 16666"
+    error_msg_is_valid = True if error_msg in process.stdout.decode() else False
+    assert error_msg_is_valid is True
+
+    # Check that the return code and standard error are correct
+    assert process.returncode == SELECT_FAILURE
+    assert process.stderr == b''
