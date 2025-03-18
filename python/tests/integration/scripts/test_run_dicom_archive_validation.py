fede4a6dbe5644d8ef6d05f0c1a17d5b9b1342f2
cmadjar <cecile.madjar@gmail.com>
2025-03-18 15:57:51 -0400

    add test to check if uploadID exists

--- python/tests/integration/scripts/test_run_dicom_archive_validation.py
+++ python/tests/integration/scripts/test_run_dicom_archive_validation.py
@@ -22,8 +22,9 @@ def reset_mri_upload_before_running(db: Database):
 def reset_tarchive_path_before_running(db: Database):
 
     tarchive = try_get_dicom_archive_with_id(db, 74)
-    tarchive.archive_location = "DCM_2015-07-07_ImagingUpload-14-30-FoTt1K.tar"
-    db.commit()
+    if tarchive:
+        tarchive.archive_location = "DCM_2015-07-07_ImagingUpload-14-30-FoTt1K.tar"
+        db.commit()
 
 
 def test_missing_upload_id_arg():
