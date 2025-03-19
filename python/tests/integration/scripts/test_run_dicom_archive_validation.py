efe249fdfb8a5ea486244f36815d6ada1ae3cf4e
cmadjar <cecile.madjar@gmail.com>
2025-03-19 12:25:42 -0400

    add test on mixed up uploadID and tarchive path

--- python/tests/integration/scripts/test_run_dicom_archive_validation.py
+++ python/tests/integration/scripts/test_run_dicom_archive_validation.py
@@ -1,7 +1,7 @@
 import subprocess
 
 from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
-from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE
+from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE, SUCCESS
 from tests.util.database import get_integration_database_session
 
 INVALID_TARCHIVE_PATH = "/data/tmp/invalid_path"
@@ -184,3 +184,30 @@ def test_mixed_up_upload_id_tarchive_path():
     # Check that the return code and standard error are correct
     assert process.returncode == SELECT_FAILURE
     assert process.stdout == b''
+
+
+def test_successful_validation():
+    db = get_integration_database_session()
+
+    # Run the script to test
+    process = subprocess.run([
+        'run_dicom_archive_validation.py',
+        '--profile', 'database_config.py',
+        '--tarchive_path', VALID_TARCHIVE_PATH,
+        '--upload_id', VALID_UPLOAD_ID,
+    ], capture_output=True)
+
+    # Print the standard output and error for debugging
+    print(f'STDOUT:\n{process.stdout.decode()}')
+    print(f'STDERR:\n{process.stderr.decode()}')
+
+    # Check that the return code and standard error are correct
+    assert process.returncode == SUCCESS
+    assert process.stderr == b''
+
+    # Check that the expected data has been inserted in the database
+    mri_upload = get_mri_upload_with_patient_name(db, 'OTT203_300203_V3')
+    assert mri_upload.inserting is False
+    assert mri_upload.is_candidate_info_validated is True
+    assert mri_upload.is_dicom_archive_validated is True
+    assert mri_upload.session is not None
