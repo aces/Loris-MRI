df6c10a2b565f5c9a0005ec000c855b407ca6c06
cmadjar <cecile.madjar@gmail.com>
2025-03-20 10:53:03 -0400

    refactorize and remove assert lib

--- python/tests/integration/scripts/test_run_dicom_archive_loader.py
+++ python/tests/integration/scripts/test_run_dicom_archive_loader.py
@@ -1,7 +1,7 @@
 from lib.db.queries.config import set_config_with_setting_name
 from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
 from lib.exitcode import SUCCESS
-from tests.util.assert_process import assert_process
+from tests.util.run_integration_script import run_integration_script
 from tests.util.database import get_integration_database_session
 from tests.util.file_system import check_file_tree
 
@@ -14,16 +14,18 @@ def test():
     db.commit()
 
     # Run the script to test
-    assert_process(
-        [
+    process = run_integration_script(
+        command=[
             'run_dicom_archive_loader.py',
             '--profile', 'database_config.py',
             '--tarchive_path', '/data/loris/tarchive/DCM_2015-07-07_ImagingUpload-14-30-FoTt1K.tar',
-        ],
-        return_code=SUCCESS,
-        stdout=None,
-        stderr=None,
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    assert process.returncode == SUCCESS
+    assert process.stdout == ""
+    assert process.stderr == ""
 
     # Check that the expected files have been created
     assert check_file_tree('/data/loris/assembly_bids', {
--- python/tests/integration/scripts/test_run_dicom_archive_validation.py
+++ python/tests/integration/scripts/test_run_dicom_archive_validation.py
@@ -1,6 +1,6 @@
 from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
 from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE, SUCCESS
-from tests.util.assert_process import assert_process
+from tests.util.run_integration_script import run_integration_script
 from tests.util.database import get_integration_database_session
 
 INVALID_TARCHIVE_PATH = "/data/tmp/invalid_path"
@@ -13,16 +13,18 @@ def test_missing_upload_id_arg():
     db = get_integration_database_session()
 
     # Run the script to test
-    assert_process(
+    process = run_integration_script(
         command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--tarchive_path', VALID_TARCHIVE_PATH,
-        ],
-        return_code=MISSING_ARG,
-        stdout="[ERROR   ] argument --upload_id is required",
-        stderr=None,
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    assert process.returncode == MISSING_ARG
+    assert "[ERROR   ] argument --upload_id is required" in process.stdout
+    assert process.stderr == ""
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'OTT203_300203_V3')
@@ -36,16 +38,18 @@ def test_missing_tarchive_path_arg():
     db = get_integration_database_session()
 
     # Run the script to test
-    assert_process(
+    process = run_integration_script(
         command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--upload_id', VALID_UPLOAD_ID,
-        ],
-        return_code=MISSING_ARG,
-        stdout="[ERROR   ] argument --tarchive_path is required",
-        stderr=None,
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    assert process.returncode == MISSING_ARG
+    assert "[ERROR   ] argument --tarchive_path is required" in process.stdout
+    assert process.stderr == ""
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'OTT203_300203_V3')
@@ -58,16 +62,18 @@ def test_missing_tarchive_path_arg():
 def test_invalid_arg():
     db = get_integration_database_session()
 
-    assert_process(
-        [
+    process = run_integration_script(
+        command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--invalid_arg',
-        ],
-        return_code=GETOPT_FAILURE,
-        stdout="option --invalid_arg not recognized",
-        stderr=None,
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    assert process.returncode == GETOPT_FAILURE
+    assert "option --invalid_arg not recognized" in process.stdout
+    assert process.stderr == ""
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'OTT203_300203_V3')
@@ -81,18 +87,21 @@ def test_invalid_tarchive_path_arg():
     db = get_integration_database_session()
 
     # Run the script to test
-    assert_process(
-        [
+    process = run_integration_script(
+        command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--tarchive_path', INVALID_TARCHIVE_PATH,
             '--upload_id', VALID_UPLOAD_ID,
-        ],
-        return_code=INVALID_PATH,
-        stdout=f"[ERROR   ] {INVALID_TARCHIVE_PATH} does not exist."
-                   f" Please provide a valid path for --tarchive_path",
-        stderr=None,
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    expected_stdout = f"[ERROR   ] {INVALID_TARCHIVE_PATH} does not exist." \
+                      f" Please provide a valid path for --tarchive_path"
+    assert process.returncode == INVALID_PATH
+    assert expected_stdout in process.stdout
+    assert process.stderr == ""
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'OTT203_300203_V3')
@@ -105,50 +114,58 @@ def test_invalid_tarchive_path_arg():
 def test_non_existent_upload_id():
 
     # Run the script to test
-    assert_process(
-        [
+    process = run_integration_script(
+        command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--tarchive_path', VALID_TARCHIVE_PATH,
             '--upload_id', INVALID_UPLOAD_ID,
-        ],
-        return_code=SELECT_FAILURE,
-        stdout=None,
-        stderr=f"ERROR: Did not find an entry in mri_upload associated with 'UploadID' {INVALID_UPLOAD_ID}",
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    expected_stderr = f"ERROR: Did not find an entry in mri_upload associated with 'UploadID' {INVALID_UPLOAD_ID}"
+    assert process.returncode == SELECT_FAILURE
+    assert process.stdout == ""
+    assert expected_stderr in process.stderr
 
 
 def test_mixed_up_upload_id_tarchive_path():
 
     # Run the script to test
-    assert_process(
-        [
+    process = run_integration_script(
+        command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--tarchive_path', VALID_TARCHIVE_PATH,
             '--upload_id', '126',
-        ],
-        return_code=SELECT_FAILURE,
-        stdout=None,
-        stderr=f"ERROR: UploadID 126 and ArchiveLocation {VALID_TARCHIVE_PATH} do not refer to the same upload"
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    expected_stderr = f"ERROR: UploadID 126 and ArchiveLocation {VALID_TARCHIVE_PATH} do not refer to the same upload"
+    assert process.returncode == SELECT_FAILURE
+    assert process.stdout == ""
+    assert expected_stderr in process.stderr
 
 
 def test_successful_validation():
     db = get_integration_database_session()
 
     # Run the script to test
-    assert_process(
-        [
+    process = run_integration_script(
+        command=[
             'run_dicom_archive_validation.py',
             '--profile', 'database_config.py',
             '--tarchive_path', VALID_TARCHIVE_PATH,
             '--upload_id', VALID_UPLOAD_ID,
-        ],
-        return_code=SUCCESS,
-        stdout=None,
-        stderr=None,
+        ]
     )
+
+    # Check return code, STDOUT and STDERR
+    assert process.returncode == SUCCESS
+    assert process.stdout == ""
+    assert process.stderr == ""
 
     # Check that the expected data has been inserted in the database
     mri_upload = get_mri_upload_with_patient_name(db, 'OTT203_300203_V3')
