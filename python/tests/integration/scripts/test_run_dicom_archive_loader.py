d50f66853b4082f7a361b324aa2b2679521a96b1
cmadjar <cecile.madjar@gmail.com>
2025-03-20 14:26:37 -0400

    add more tests for dicom_archive_loader

--- python/tests/integration/scripts/test_run_dicom_archive_loader.py
+++ python/tests/integration/scripts/test_run_dicom_archive_loader.py
@@ -1,12 +1,77 @@
 from lib.db.queries.config import set_config_with_setting_name
 from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
-from lib.exitcode import SUCCESS
+from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, SELECT_FAILURE, SUCCESS
 from tests.util.database import get_integration_database_session
 from tests.util.file_system import check_file_tree
 from tests.util.run_integration_script import run_integration_script
 
 
-def test():
+def test_invalid_arg():
+    db = get_integration_database_session()
+
+    process = run_integration_script(
+        command=[
+            'run_dicom_archive_loader.py',
+            '--profile', 'database_config.py',
+            '--invalid_arg',
+        ]
+    )
+
+    # Check return code, STDOUT and STDERR
+    assert process.returncode == GETOPT_FAILURE
+    assert "option --invalid_arg not recognized" in process.stdout
+    assert process.stderr == ""
+
+    # Check that the expected data has been inserted in the database
+    mri_upload = get_mri_upload_with_patient_name(db, 'MTL001_300001_V2')
+    assert mri_upload.inserting is False
+    assert mri_upload.is_candidate_info_validated is False
+    assert mri_upload.is_dicom_archive_validated is False
+    assert mri_upload.session is None
+
+
+def test_non_existent_upload_id():
+
+    invalid_upload_id = '16666'
+
+    # Run the script to test
+    process = run_integration_script(
+        command=[
+            'run_dicom_archive_validation.py',
+            '--profile', 'database_config.py',
+            '--upload_id', invalid_upload_id,
+        ]
+    )
+
+    # Check return code, STDOUT and STDERR
+    expected_stderr = f"ERROR: Did not find an entry in mri_upload associated with 'UploadID' {invalid_upload_id}"
+    assert process.returncode == SELECT_FAILURE
+    assert process.stdout == ""
+    assert expected_stderr in process.stderr
+
+
+def test_invalid_tarchive_path_arg():
+
+    invalid_tarchive_path = "/data/tmp/invalid_path"
+
+    # Run the script to test
+    process = run_integration_script(
+        command=[
+            'run_dicom_archive_loader.py',
+            '--profile', 'database_config.py',
+            '--tarchive_path', invalid_tarchive_path,
+        ]
+    )
+
+    # Check return code, STDOUT and STDERR
+    expected_stdout = f"[ERROR   ] {invalid_tarchive_path} does not exist." \
+                      f" Please provide a valid path for --tarchive_path"
+    assert process.returncode == INVALID_PATH
+    assert expected_stdout in process.stdout
+    assert process.stderr == ""
+
+
+def test_successful_run_on_valid_tarchive_path():
     db = get_integration_database_session()
 
     # Set the configuration to use the DICOM to BIDS pipeline
@@ -40,9 +105,15 @@ def test():
     })
 
     # Check that the expected data has been inserted in the database
+    archive_new_path = '/data/loris/tarchive/2015/DCM_2015-07-07_ImagingUpload-14-30-FoTt1K.tar'
     mri_upload = get_mri_upload_with_patient_name(db, 'MTL001_300001_V2')
     assert mri_upload.inserting is False
+    assert mri_upload.insertion_complete is True
     assert mri_upload.is_candidate_info_validated is True
     assert mri_upload.is_dicom_archive_validated is True
+    assert mri_upload.number_of_minc_inserted == 1
+    assert mri_upload.number_of_minc_created == 1
     assert mri_upload.session is not None
+    assert mri_upload.dicom_archive.session_id is not None
+    assert mri_upload.dicom_archive.archive_location == archive_new_path
     assert len(mri_upload.session.files) == 1
--- test/RB_SQL/RB_tarchive_series.sql
+++ test/RB_SQL/RB_tarchive_series.sql
@@ -1,7 +1,8 @@
 SET FOREIGN_KEY_CHECKS=0;
 TRUNCATE TABLE `tarchive_series`;
 LOCK TABLES `tarchive_series` WRITE;
-INSERT INTO `tarchive_series` (`TarchiveSeriesID`, `TarchiveID`, `SeriesNumber`, `SeriesDescription`, `SequenceName`, `EchoTime`, `RepetitionTime`, `InversionTime`, `SliceThickness`, `PhaseEncoding`, `NumberOfFiles`, `SeriesUID`, `Modality`) VALUES (5678,74,2,'t1_mpr_1mm_p2','*tfl3d1_ns',3.16,2400,1200,1,'ROW',224,'1.3.12.2.1107.5.2.32.35045.2015070800014525082741283.0.0.0','MR');
 INSERT INTO `tarchive_series` (`TarchiveSeriesID`, `TarchiveID`, `SeriesNumber`, `SeriesDescription`, `SequenceName`, `EchoTime`, `RepetitionTime`, `InversionTime`, `SliceThickness`, `PhaseEncoding`, `NumberOfFiles`, `SeriesUID`, `Modality`) VALUES (5679,75,2,'t1_mpr_1mm_p2_pos50','*tfl3d1_ns',3.16,2400,1200,1,'ROW',160,'1.3.12.2.1107.5.2.32.35248.2017100418274954307032511.0.0.0','MR');
+INSERT INTO `tarchive_series` (`TarchiveSeriesID`, `TarchiveID`, `SeriesNumber`, `SeriesDescription`, `SequenceName`, `EchoTime`, `RepetitionTime`, `InversionTime`, `SliceThickness`, `PhaseEncoding`, `NumberOfFiles`, `SeriesUID`, `Modality`) VALUES (5779,74,1,'localizer','*fl2d1',5,20,NULL,10,'COL',1,'1.3.12.2.1107.5.2.32.35045.2015070723561020366340472.0.0.0','MR');
+INSERT INTO `tarchive_series` (`TarchiveSeriesID`, `TarchiveID`, `SeriesNumber`, `SeriesDescription`, `SequenceName`, `EchoTime`, `RepetitionTime`, `InversionTime`, `SliceThickness`, `PhaseEncoding`, `NumberOfFiles`, `SeriesUID`, `Modality`) VALUES (5780,74,2,'t1_mpr_1mm_p2','*tfl3d1_ns',3.16,2400,1200,1,'ROW',224,'1.3.12.2.1107.5.2.32.35045.2015070800014525082741283.0.0.0','MR');
 UNLOCK TABLES;
 SET FOREIGN_KEY_CHECKS=1;
