68a02dbe00b468de55bec355711e0c070567614f
cmadjar <cecile.madjar@gmail.com>
2025-04-02 15:28:35 -0400

    fix tests + typo in error message of run_nifti_insertion.py

--- python/tests/integration/scripts/test_run_nifti_insertion.py
+++ python/tests/integration/scripts/test_run_nifti_insertion.py
@@ -1,8 +1,4 @@
-from lib.db.queries.config import set_config_with_setting_name
-from lib.db.queries.mri_upload import get_mri_upload_with_patient_name
 from lib.exitcode import GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE, SUCCESS
-from tests.util.database import get_integration_database_session
-from tests.util.file_system import check_file_tree
 from tests.util.run_integration_script import run_integration_script
 
 
@@ -53,7 +49,7 @@ def test_invalid_nifti_path():
     )
 
     # Check return code, STDOUT and STDERR
-    expected_stdout = f"[ERROR   ] {nifti_path} does not exits. Please provide a valid path for --nifti_path"
+    expected_stdout = f"[ERROR   ] {nifti_path} does not exist. Please provide a valid path for --nifti_path"
     assert process.returncode == INVALID_PATH
     assert expected_stdout in process.stdout
     assert process.stderr == ""
@@ -146,7 +142,59 @@ def test_invalid_upload_id():
     )
 
     # Check return code, STDOUT and STDERR
-    expected_stderr = f"[ERROR   ] Did not find an entry in mri_upload associated with 'UploadID' {upload_id}"
+    expected_stderr = f"ERROR: Did not find an entry in mri_upload associated with 'UploadID' {upload_id}"
     assert process.returncode == SELECT_FAILURE
     assert expected_stderr in process.stderr
     assert process.stdout == ""
+
+
+def test_invalid_tarchive_path():
+
+    nifti_path = '/data/loris/incoming/niftis/MTL001_300001_V2_t1_valid.nii.gz'
+    json_path = '/data/loris/incoming/niftis/MTL001_300001_V2_t1_valid.json'
+    tarchive_path = '/data/tmp/non-existent-tarchive.tgz'
+
+    # Run the script to test
+    process = run_integration_script(
+        command=[
+            'run_nifti_insertion.py',
+            '--profile', 'database_config.py',
+            '--nifti_path', nifti_path,
+            '--tarchive_path', tarchive_path,
+            '--json_path', json_path,
+        ]
+    )
+
+    # Check return code, STDOUT and STDERR
+    expected_stdout = f"[ERROR   ] {tarchive_path} does not exist. Please provide a valid path for --tarchive_path"
+    assert process.returncode == INVALID_PATH
+    assert expected_stdout in process.stdout
+    assert process.stderr == ""
+
+
+def test_tarchive_path_and_upload_id_provided():
+
+    nifti_path = '/data/loris/incoming/niftis/MTL001_300001_V2_t1_valid.nii.gz'
+    json_path = '/data/loris/incoming/niftis/MTL001_300001_V2_t1_valid.json'
+    tarchive_path = '/data/tmp/non-existent-tarchive.tgz'
+    upload_id = '126'
+
+    # Run the script to test
+    process = run_integration_script(
+        command=[
+            'run_nifti_insertion.py',
+            '--profile', 'database_config.py',
+            '--nifti_path', nifti_path,
+            '--tarchive_path', tarchive_path,
+            '--upload_id', upload_id,
+            '--json_path', json_path,
+        ]
+    )
+
+    # Check return code, STDOUT and STDERR
+    expected_stdout = f"ERROR   ] You should either specify an upload_id or a tarchive_path or use the -force option" \
+                      f" (if no upload_id or tarchive_path is available for the NIfTI file to be uploaded)." \
+                      f" Make sure that you set only one of those options. Upload will exit now."
+    assert process.returncode == GETOPT_FAILURE
+    assert expected_stdout in process.stdout
+    assert process.stderr == ""
