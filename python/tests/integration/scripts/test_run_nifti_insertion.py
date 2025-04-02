5c8a39b332b6f0a91173afba16d31c85cfaa7f63
cmadjar <cecile.madjar@gmail.com>
2025-04-02 16:44:06 -0400

    fix tests with new files

--- python/tests/integration/scripts/test_run_nifti_insertion.py
+++ python/tests/integration/scripts/test_run_nifti_insertion.py
@@ -1,4 +1,4 @@
-from lib.exitcode import FILENAME_MISMATCH, GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE, SUCCESS
+from lib.exitcode import FILENAME_MISMATCH, FILE_NOT_UNIQUE, GETOPT_FAILURE, INVALID_PATH, MISSING_ARG, SELECT_FAILURE
 from tests.util.run_integration_script import run_integration_script
 
 
@@ -202,7 +202,7 @@ def test_tarchive_path_and_upload_id_provided():
 def test_nifti_and_tarchive_patient_name_differ():
 
     nifti_path = '/data/loris/incoming/niftis/ROM184_400184_V3_t2_invalid_pname.nii.gz'
-    json_path = '/data/loris/incoming/niftis/ROM184_400184_V3_t2_invalid_pname.nii.gz'
+    json_path = '/data/loris/incoming/niftis/ROM184_400184_V3_t2_invalid_pname.json'
     upload_id = '128'
 
     # Run the script to test
@@ -219,5 +219,32 @@ def test_nifti_and_tarchive_patient_name_differ():
     # Check return code, STDOUT and STDERR
     expected_stderr = "ERROR: PatientName in DICOM and NIfTI files differ."
     assert process.returncode == FILENAME_MISMATCH
+    assert expected_stderr in process.stderr
+    assert process.stdout == ""
+
+
+def test_nifti_already_uploaded():
+
+    series_uid = '1.3.12.2.1107.5.2.32.35412.2012101116562350450995317.0.0.0'
+    nifti_path = '/data/loris/assembly_bids/sub-400184/ses-V3/func/sub-400184_ses-V3_task-rest_run-1_bold.nii.gz'
+    json_path = '/data/loris/assembly_bids/sub-400184/ses-V3/func/sub-400184_ses-V3_task-rest_run-1_bold.json'
+    upload_id = '128'
+
+    # Run the script to test
+    process = run_integration_script(
+        command=[
+            'run_nifti_insertion.py',
+            '--profile', 'database_config.py',
+            '--nifti_path', nifti_path,
+            '--upload_id', upload_id,
+            '--json_path', json_path,
+        ]
+    )
+
+    # Check return code, STDOUT and STDERR
+    expected_stderr = f"ERROR: There is already a file registered in the files table with SeriesUID {series_uid}," \
+                      f" EchoTime 0.027, EchoNumber None and PhaseEncodingDirection j-. The already registered" \
+                      f" file is {nifti_path.replace('/data/loris/', '')}"
+    assert process.returncode == FILE_NOT_UNIQUE
     assert expected_stderr in process.stderr
     assert process.stdout == ""
