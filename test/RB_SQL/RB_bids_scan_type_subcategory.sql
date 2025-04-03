b091497dbc27c2be423ba955d9259baa00c32b18
cmadjar <cecile.madjar@gmail.com>
2025-04-03 17:22:19 -0400

    add bids rel files to support DWI 25 for testing

---
+++ test/RB_SQL/RB_bids_scan_type_subcategory.sql
@@ -0,0 +1,7 @@
+SET FOREIGN_KEY_CHECKS=0;
+TRUNCATE TABLE `bids_scan_type_subcategory`;
+LOCK TABLES `bids_scan_type_subcategory` WRITE;
+INSERT INTO `bids_scan_type_subcategory` (`BIDSScanTypeSubCategoryID`, `BIDSScanTypeSubCategory`) VALUES (2,'acq-25dir');
+INSERT INTO `bids_scan_type_subcategory` (`BIDSScanTypeSubCategoryID`, `BIDSScanTypeSubCategory`) VALUES (1,'task-rest');
+UNLOCK TABLES;
+SET FOREIGN_KEY_CHECKS=1;
