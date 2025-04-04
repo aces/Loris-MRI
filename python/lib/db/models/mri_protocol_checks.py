62d70b9712f16b25334c8e333a0174636e32aa72
cmadjar <cecile.madjar@gmail.com>
2025-04-03 21:14:32 -0400

    add query to parameter_type and parameter_file to check that files got properly inserted

--- python/lib/db/models/mri_protocol_checks.py
+++ python/lib/db/models/mri_protocol_checks.py
@@ -11,15 +11,15 @@ from lib.db.base import Base
 class DbMriProtocolChecks(Base):
     __tablename__ = 'mri_protocol_checks'
 
-    id                          : Mapped[int]          = mapped_column('ID', primary_key=True)
-    mri_scan_type_id            : Mapped[int | None]   \
+    id                          : Mapped[int]            = mapped_column('ID', primary_key=True)
+    mri_scan_type_id            : Mapped[int | None] \
         = mapped_column('MriScanTypeID', ForeignKey('mri_scan_type.MriScanTypeID'))
-    severity                    : Mapped[str | None]   = mapped_column('Severity')
-    header                      : Mapped[str | None]   = mapped_column('Header')
+    severity                    : Mapped[str | None]     = mapped_column('Severity')
+    header                      : Mapped[str | None]     = mapped_column('Header')
     valid_min                   : Mapped[Decimal | None] = mapped_column('ValidMin')
     valid_max                   : Mapped[Decimal | None] = mapped_column('ValidMax')
-    valid_regex                 : Mapped[str | None]   = mapped_column('ValidRegex')
-    mri_protocol_group_check_id : Mapped[int | None]   \
+    valid_regex                 : Mapped[str | None]     = mapped_column('ValidRegex')
+    mri_protocol_checks_group_id: Mapped[int] \
         = mapped_column('MriProtocolChecksGroupID', ForeignKey('mri_protocol_checks_group.MriProtocolChecksGroupID'))
 
     scan_type            : Mapped['db_mri_scan_type.DbMriScanType'] \
