bacb2309af11f39ab4e099ed25029e3ed3d9be76
Maxime Mulder <maxime.mulder@affiliate.mcgill.ca>
2026-01-05 12:02:57 +0000

    migrate scan type as well

--- python/lib/imaging_lib/file.py
+++ python/lib/imaging_lib/file.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from lib.db.models.dicom_archive import DbDicomArchive
 from lib.db.models.file import DbFile
+from lib.db.models.mri_scan_type import DbMriScanType
 from lib.db.models.mri_scanner import DbMriScanner
 from lib.db.models.session import DbSession
 from lib.env import Env
@@ -14,7 +15,7 @@ def register_mri_file(
     file_type: str,
     file_path: Path,
     session: DbSession,
-    scan_type_id: int | None,
+    scan_type: DbMriScanType | None,
     scanner: DbMriScanner | None,
     dicom_archive: DbDicomArchive | None,
     series_instance_uid: str | None,
@@ -44,7 +45,7 @@ def register_mri_file(
         echo_number              = echo_number,
         phase_encoding_direction = phase_encoding_direction,
         source_file_id           = None,
-        scan_type_id             = scan_type_id,
+        scan_type_id             = scan_type.id if scan_type is not None else None,
         scanner_id               = scanner.id if scanner is not None else None,
         dicom_archive_id         = dicom_archive.id if dicom_archive is not None else None,
         caveat                   = caveat,
