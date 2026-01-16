ca724447af40d774bd59ebc04fb435c6fa90eede
Maxime Mulder <maxime.mulder@affiliate.mcgill.ca>
2026-01-16 09:22:00 +0000

    bids eeg sidecar reader

---
+++ python/loris_bids_reader/src/loris_bids_reader/eeg/sidecar.py
@@ -0,0 +1,13 @@
+from loris_bids_reader.json import BidsJsonFile
+
+
+class BidsEegSidecarJsonFile(BidsJsonFile):
+    """
+    Class representing a BIDS EEG or iEEG sidecar JSON file.
+
+    Documentation:
+    - https://bids-specification.readthedocs.io/en/stable/modality-specific-files/electroencephalography.html#sidecar-json-_eegjson
+    - https://bids-specification.readthedocs.io/en/stable/modality-specific-files/intracranial-electroencephalography.html#sidecar-json-_ieegjson
+    """
+
+    pass
